b4f3010667b1ffce1f7e2f875c84104e71a2cb9b
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2023-12-17 23:48:55 +0300

    fr ButtonBuyInTable and ButtonSellInTable classes

--- pages/Elements/ButtonBuyInTable.py
+++ pages/Elements/ButtonBuyInTable.py
@@ -16,7 +16,7 @@ from pages.Elements.AssertClass import AssertClass
 
 class BuyButtonTable(BasePage):
 
-    def full_test(self, d, cur_language, cur_country, cur_role, cur_item_link, cur_tab):
+    def full_test(self, d, cur_language, cur_country, cur_role, cur_item_link, cur_type_fi, cur_tab):
         self.arrange_(d, cur_item_link, cur_tab)
         self.element_click(cur_item_link, cur_language, cur_role, cur_tab)
 
