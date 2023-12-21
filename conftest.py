f6d33f91d445a2aa9f319659adfac3eaa609842f
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2023-12-21 04:28:13 +0300

    set cn/gb

--- conftest.py
+++ conftest.py
@@ -51,8 +51,8 @@ def pytest_addoption(parser):
     scope="class",
     params=[
         "NoReg",
-        # "Auth",
-        # "NoAuth",  # "Reg/NoAuth"
+        "Auth",
+        "NoAuth",  # "Reg/NoAuth"
     ],
 )
 def cur_role(request):
@@ -76,14 +76,14 @@ def cur_role(request):
         # "de",  # 15 us
         # "it",  # 15 us
         # "ru",  # 15 us
-        # "cn",  # 13 us Education to trade present, financial glossary not present
+        "cn",  # 13 us Education to trade present, financial glossary not present
         # "zh",  # 12 us
         # "fr",  # 11 us
         # "pl",  # 10 us
         # "ro",  # 10 us
         # "ar",  # 8 us
         # "nl",  # 8 us
-        "el",  # 5 us
+        # "el",  # 5 us
         # "hu",  # 5 us Magyar
     ],
 )
@@ -103,12 +103,12 @@ def cur_language(request):
 @pytest.fixture(
     scope="class",
     params=[
-        # "gb",  # United Kingdom - "FCA"
+        "gb",  # United Kingdom - "FCA"
         # "au",  # Australia - "ASIC"
         # "de",  # Germany - "CYSEC"
         # "ae",  # United Arab Emirates - "SCB"
 
-        "gr",  # Greece - "CYSEC"
+        # "gr",  # Greece - "CYSEC"
         # "es",  # Spain - "CYSEC"
         # "fr",  # France - "CYSEC"
         # "it",  # Italy - "CYSEC"
