2f1e5d1d0f16204c262bdb79372876ac52410818
MikeTaran <mtaran.job@gmail.com>
2023-12-22 15:58:15 +0200

    refactor: reduce qty of GSAPI requests

--- tests/ReTests/GoogleSheets/googlesheets.py
+++ tests/ReTests/GoogleSheets/googlesheets.py
@@ -16,7 +16,7 @@ class GoogleSheet:
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    # RANGE_NAME = f"BugsReport!A4:O4"
+
     RANGE_NAME = "BugsReport!A4:P4"
     # If modifying these scopes, delete the file token.json.
     SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
@@ -59,8 +59,8 @@ class GoogleSheet:
                 return sheet['properties']['sheetId']
         return None  # Если лист не найден
 
-    def getRangeValues(self, num_row=4):
-        RANGE_NAME = f"{self.SHEET_NAME}!A{num_row}:P{num_row}"
+    def getRangeValues(self, end_row=4):
+        RANGE_NAME = f"{self.SHEET_NAME}!A4:P{end_row}"
         # Call the Sheets API
         sheet = self.service.spreadsheets()
         result = (
@@ -72,21 +72,7 @@ class GoogleSheet:
 
         return values
 
-    def updateRangeValues(self, num_cell=4, values=""):
-        RANGE_NAME = f'{self.SHEET_NAME}!V{num_cell}'
-        data = [{
-            'range': RANGE_NAME,
-            'values': values
-        }]
-        body = {
-            'valueInputOption': 'USER_ENTERED',
-            'data': data
-        }
-        result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.SPREADSHEET_ID,
-                                                                  body=body).execute()
-        print('{0} cells updated.'.format(result.get('totalUpdatedCells')))
-
-    def putRangeValues(self, cell, values=""):
+    def updateRangeValues(self, cell='V4', values=""):
         RANGE_NAME = f'{self.SHEET_NAME}!{cell}'
         data = [{
             'range': RANGE_NAME,
@@ -98,7 +84,7 @@ class GoogleSheet:
         }
         result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.SPREADSHEET_ID,
                                                                   body=body).execute()
-        # print('{0} cells updated.'.format(result.get('totalUpdatedCells')))
+        return result
 
     def add_new_column_after_(self, index_of_col=21):
         print("Добавление нового столбца")
@@ -118,24 +104,5 @@ class GoogleSheet:
             self.service.spreadsheets().batchUpdate(spreadsheetId=self.SPREADSHEET_ID,
                                                     body=request_body).execute()
 
-            # Обновление значений для нового столбца
-            # new_column_values = [[value] for value in new_column_values]
-
-            # Обновление данных в новом столбце
-            # self.update_range_values(sheet_name, spreadsheet_id, f'{chr(65 + index_of_R)}1',
-            #                          new_column_values)  # chr(65) = 'A', chr(66) = 'B', и т.д.
         else:
             print(f"Столбец {index_of_col} не найден в таблице.")
-
-    # def update_range_values(self, sheet_name, spreadsheet_id, range_name, values):
-    #     range_ = f'{sheet_name}!{range_name}'
-    #     data = [{
-    #         'range': range_,
-    #         'values': values
-    #     }]
-    #     body = {
-    #         'valueInputOption': 'USER_ENTERED',
-    #         'data': data
-    #     }
-    #     result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet_id, body=body).execute()
-    #     print('{0} cells updated.'.format(result.get('totalUpdatedCells')))
