3d46a18588cd06302a567dcd6affbbf69ab2c344
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2023-12-23 13:13:36 +0300

    rf buils_dynamic_arg_v4

--- tests/build_dynamic_arg.py
+++ tests/build_dynamic_arg.py
@@ -28,12 +28,8 @@ def build_dynamic_arg_v4(d, worker_id, cur_language, cur_country, cur_role,
     # language = cur_language
     if cur_language == "":
         cur_language = "en"
-    # dynamic_epic = f"US_{us} | {desc_feature} / Language: {language}"
     dynamic_epic = f"US_{us} | {desc_us}"
-    # dynamic_feature = f"Country: {cur_country} / Role: {cur_role} / TS_{us} | {desc_feature}"
     dynamic_feature = f"Language: {cur_language}"
-    # dynamic_story = f"TC_{us}{num_tc} | {desc_story}"
-    # dynamic_story = f"Country: {cur_country} / Role: {cur_role} / TC_{us}{num_tc} | {desc_tc}"
     dynamic_story = f"Country: {cur_country} / Role: {cur_role}"
     bug_id = f"bid:{us}{num_tc}-{cur_language}{cur_country}{cur_role}"
 
