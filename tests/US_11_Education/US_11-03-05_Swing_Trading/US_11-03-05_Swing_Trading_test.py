f8b8379f6754a76dae47e1eefdb35f5c8806f308
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-01-05 13:30:15 +0300

    rf us_11.03.05

--- tests/US_11_Education/US_11-03-05_Swing_Trading/US_11-03-05_Swing_Trading_test.py
+++ tests/US_11_Education/US_11-03-05_Swing_Trading/US_11-03-05_Swing_Trading_test.py
@@ -97,7 +97,7 @@ class TestSwingTrading:
             case "NoAuth":
                 test_element.assert_login(d, cur_language, link)
             case "Auth":
-                test_element.assert_trading_platform_v4(d, link, demo=True)
+                test_element.assert_trading_platform_v4(d, link, True)
 
     @allure.step("Start test of buttons [Trade] in Most traded block")
     @pytest.mark.test_03
