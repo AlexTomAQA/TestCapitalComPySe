93648ef1675ba7c99965bc971418550f23a534fc
Aleksandr Tomelo <146713192+AlexTomAQA@users.noreply.github.com>
2024-01-26 00:50:17 +0300

    set en/gb

--- conftest.py
+++ conftest.py
@@ -95,7 +95,7 @@ def cur_role(request):
         # "zh",  # 12 us
         # "fr",  # 11 us
         # "pl",  # 10 us
-        "ro",  # 10 us
+        # "ro",  # 10 us
         # "ar",  # 8 us
         # "nl",  # 8 us
         # "el",  # 5 us
