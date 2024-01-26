ffe5ba0d642d85d4539a84fa6b52fd8fa185e796
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-01-26 22:56:09 +0300

    rf captcha class

--- pages/captcha.py
+++ pages/captcha.py
@@ -5,8 +5,10 @@
 """
 from datetime import datetime
 
+import pytest
 import allure
 from selenium.webdriver.common.by import By
+
 from pages.base_page import BasePage
 
 
@@ -16,15 +18,17 @@ class Captcha(BasePage):
     LOCATOR = "div.g-recaptcha[data-sitekey]"
 
     @allure.step("Start Checking Captcha")
-    def is_captcha_v2(self, driver):
-        captcha = driver.find_elements(By.CSS_SELECTOR, self.LOCATOR)
+    def fail_test_if_captcha_present_v2(self):
+        captcha = self.driver.find_elements(By.CSS_SELECTOR, self.LOCATOR)
         if len(captcha) == 0:
-            print(f"\n{datetime.now()}   Капчи нет. Тестим дальше")
-            return False
-        return True
+            print(f"\n{datetime.now()}   Капчи нет. Идем дальше")
+            return
 
-    def print_env(self, driver):
-        data_site_key = driver.find_elements(By.CSS_SELECTOR, self.LOCATOR)[0].get_property("data-sitekey")
-        captcha_page_url = driver.current_url
+        self.print_env()
+        pytest.fail("reCaptcha V2")
+
+    def print_env(self):
+        data_site_key = self.driver.find_elements(By.CSS_SELECTOR, self.LOCATOR)[0].get_property("data-sitekey")
+        captcha_page_url = self.driver.current_url
         print(f"{datetime.now()}   На странице {captcha_page_url} проявилась Captcha")
         print(f"{datetime.now()}   SiteKey = {data_site_key}")
