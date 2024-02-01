31c0c439376444e21d8e22e7905d1ee26fbdab00
Amid-Kirdum <mudrik1200@gmail.com>
2024-02-02 02:01:45 +0300

    add test [Start Trading] button_01

--- tests/US_11_Education/US_11-02-05_Cryptocurrency_trading/us_11-02-05-00_cryptocurrency_trading_test.py
+++ tests/US_11_Education/US_11-02-05_Cryptocurrency_trading/us_11-02-05-00_cryptocurrency_trading_test.py
@@ -3,15 +3,17 @@
 @Time    : 2023/05/23 19:00 GMT+3
 @Author  : Suleyman Alirzaev
 """
+import allure
 # import os.path
 import pytest
-import allure
-from pages.common import Common
+
+from pages.Elements.ButtonStartTradingMainBanner import MainBannerStartTrading
+from pages.Elements.testing_elements_locators import SubPages
 from pages.Menu.menu import MenuSection
-from tests.build_dynamic_arg import build_dynamic_arg_v4
+from pages.common import Common
 from pages.conditions import Conditions
 from src.src import CapitalComPageSrc
-from pages.Elements.testing_elements_locators import SubPages
+from tests.build_dynamic_arg import build_dynamic_arg_v4
 
 count = 1
 
@@ -62,3 +64,41 @@ class TestCryptocurrencyTradingPretest:
         Common().creating_file_of_hrefs("Cryptocurrency trading", list_items, file_name)
 
         count -= 1
+
+
+@pytest.mark.us_11_02_05
+class TestCryptocurrencyTrading:
+    page_conditions = None
+
+    def check_language(self, cur_language):
+        if cur_language not in ["ar", "el", "hu", "nl"]:
+            return
+        pytest.skip(f"This test is not for {cur_language} language")
+
+    @allure.step("Test button [Start Trading] on the Main banner")
+    @pytest.mark.test_01
+    def test_01_main_banner_start_trading_button(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+        Check: Button [Start Trading] on Main banner
+        Language: All, except AR, EL, HU, NL.
+        License: All, except FCA (GB country)
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "11.02.05", "Education > Menu item [Cryptocurrency trading]",
+            ".00_01", "Testing button [Start Trading] on the Main banner")
+
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+
+        self.check_language(cur_language)
+
+        page_conditions = Conditions(d, "")
+        main_page_link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, main_page_link)
+        cur_menu_link = page_menu.open_education_shares_trading_menu(d, cur_language, cur_country, main_page_link)
+
+        test_element = MainBannerStartTrading(d, cur_menu_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_menu_link)
