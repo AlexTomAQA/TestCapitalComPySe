0af8a0388b0a5106192a21b331e9391fc6f5ba5c
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-02 16:49:06 +0300

    set de/au fo Retests

--- tests/ReTests/conftest.py
+++ tests/ReTests/conftest.py
@@ -41,8 +41,8 @@ role_list = [
 # ===========================================================
 # выбор необходимых лицензий для ретеста
 country_list = [
-        "gb",  # United Kingdom - "FCA"
-        # "au",  # Australia - "ASIC"
+        # "gb",  # United Kingdom - "FCA"
+        "au",  # Australia - "ASIC"
         # "de",  # Germany - "CYSEC"
         # "ae",  # United Arab Emirates - "SCB"
 ]
