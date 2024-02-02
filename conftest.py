11c702a93f794f97f3981dbcd27295561ba600d7
Aleksandr Tomelo <146713192+AlexTomAQA@users.noreply.github.com>
2024-02-03 01:27:01 +0300

    set zh/de

--- conftest.py
+++ conftest.py
@@ -125,9 +125,9 @@ def cur_language(request):
 @pytest.fixture(
     scope="class",
     params=[
-        "gb",  # United Kingdom - "FCA"
+        # "gb",  # United Kingdom - "FCA"
+        "de",  # Germany - "CYSEC"
         # "au",  # Australia - "ASIC"
-        # "de",  # Germany - "CYSEC"
         # "ae",  # United Arab Emirates - "SCB"
 
         # "gr",  # Greece - "CYSEC"
