431ad914b2925fe3b7621ba43d8a89cb4b4075ca
Amid-Kirdum <mudrik1200@gmail.com>
2024-02-03 23:18:59 +0300

    add AT_03 > trade button

--- tests/US_11_Education/US_11-02-05_Cryptocurrency_trading/us_11-02-05-00_cryptocurrency_trading_test.py
+++ tests/US_11_Education/US_11-02-05_Cryptocurrency_trading/us_11-02-05-00_cryptocurrency_trading_test.py
@@ -8,6 +8,7 @@ import allure
 import pytest
 
 from pages.Elements.ButtonStartTradingMainBanner import MainBannerStartTrading
+from pages.Elements.ButtonTradeOnWidgetMostTraded import ButtonTradeOnWidgetMostTraded
 from pages.Elements.ButtonTryDemoMainBanner import MainBannerTryDemo
 from pages.Elements.testing_elements_locators import SubPages
 from pages.Menu.menu import MenuSection
@@ -100,7 +101,7 @@ class TestCryptocurrencyTrading:
         test_element = MainBannerStartTrading(d, cur_menu_link, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_menu_link)
 
-    @allure.step("Test button [Try demo] on the MAin banner")
+    @allure.step("Test button [Try demo] on the Main banner")
     @pytest.mark.test_02
     def test_02_main_banner_try_demo_button(
             self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
@@ -111,7 +112,7 @@ class TestCryptocurrencyTrading:
         """
         bid = build_dynamic_arg_v4(
             d, worker_id, cur_language, cur_country, cur_role,
-            "11.02.05","Education > Menu item [Cryptocurrency trading]",
+            "11.02.05", "Education > Menu item [Cryptocurrency trading]",
             ".00_02", "Testing button [Try demo] on the Main banner")
 
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
@@ -130,7 +131,32 @@ class TestCryptocurrencyTrading:
         test_element = MainBannerTryDemo(d, cur_menu_link, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_menu_link)
 
+    @allure.step("Test button [Trade] on widget Most traded")
+    @pytest.mark.test_03
+    def test_03_most_traded_widget_trade_button(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+        Check: Button [Trade] on widget Most traded
+        Language: All, except AR, EL, HU, NL.
+        License: All, except FCA (GB country)
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "11.02.05", "Education > Menu item [Cryptocurrency trading]",
+            ".00_03", "Testing button [Trade] on widget Most traded")
 
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+        Common().check_country_in_list_and_skip_if_present(
+            cur_language, ["ar", "el", "hu", "nl"])
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
 
+        page_conditions = Conditions(d, "")
+        main_page_link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
 
+        page_menu = MenuSection(d, main_page_link)
+        cur_menu_link = page_menu.open_education_cryptocurrency_trading_menu(
+            d, cur_language, cur_country, main_page_link)
 
+        test_element = ButtonTradeOnWidgetMostTraded(d, cur_menu_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_menu_link)
