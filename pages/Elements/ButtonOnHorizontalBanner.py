49374664ebf019c7f54aa8f2ae1be05c35dc25b9
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-06 12:59:37 +0300

    rf ButtonOnHorizontalBanner and ButtonOnVerticalBanner

--- pages/Elements/ButtonOnHorizontalBanner.py
+++ pages/Elements/ButtonOnHorizontalBanner.py
@@ -86,25 +86,17 @@ class ButtonOnHorizontalBanner(BasePage):
     def element_click(self):
         print(f"\n{datetime.now()}   2. Act")
 
-        # print(f"{datetime.now()}   BUTTON_ON_HOR_BANNER is present? =>")
-        # button_list = self.driver.find_elements(*ButtonOnHorizontalBannerLocators.BUTTON_ON_HOR_BANNER)
-        # if len(button_list) == 0:
-        #     print(f"{datetime.now()}   => BUTTON_ON_HOR_BANNER is not present on the page!")
-        #     del button_list
-        #     return False
-        # print(f"{datetime.now()}   => BUTTON_ON_HOR_BANNER is present on the page")
-        #
-        # print(f"{datetime.now()}   BUTTON_ON_HOR_BANNER scroll =>")
-        # self.driver.execute_script(
-        #     'return arguments[0].scrollIntoView({block: "center", inline: "nearest"});',
-        #     button_list[0]
-        # )
-
+        time_out = 5
         button_list = self.driver.find_elements(*ButtonOnHorizontalBannerLocators.BUTTON_ON_HOR_BANNER)
-        self.element_is_clickable(button_list[0], 5)
+        web_element = self.element_is_clickable(button_list[0], time_out)
+        if not web_element:
+            print(f"\n{datetime.now()}   => Button on Horizontal banner not clickable after {time_out} sec.")
+            pytest.fail(f"Button on Horizontal banner not clickable after {time_out} sec.")
+        print(f"\n{datetime.now()}   => Button on Horizontal banner clickable")
 
         data_type = button_list[0].get_attribute("data-type")
         data_id = data_type.split('_')[-1]
+        print(f"\n{datetime.now()}   data_id = {data_id}")
 
         try:
             button_list[0].click()
@@ -113,14 +105,15 @@ class ButtonOnHorizontalBanner(BasePage):
             print(f"{datetime.now()}   => BUTTON_ON_HOR_BANNER NOT CLICKED")
             print(f"{datetime.now()}   'Sign up' form or page is auto opened")
 
-            page_ = SignupLogin(self.driver)
-            if page_.close_signup_form():
-                pass
-            else:
-                page_.close_signup_page()
+            # page_ = SignupLogin(self.driver)
+            # if page_.close_signup_form():
+            #     pass
+            # else:
+            #     page_.close_signup_page()
+            #
+            # button_list[0].click()
+            # del page_
 
-            button_list[0].click()
-            del page_
-
+        del web_element
         del button_list
         return data_id
