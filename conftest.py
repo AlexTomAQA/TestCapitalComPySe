a5da23d64c52c5326cb2acf9d883e4e8bdc1dede
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-12 23:27:40 +0300

    set ro/gb/all

--- conftest.py
+++ conftest.py
@@ -87,7 +87,7 @@ def cur_role(request):
     scope="class",
     params=[
         # "",  # "en" - 21 us
-        "es",  # 20 us
+        # "es",  # 20 us
         # "de",  # 15 us
         # "it",  # 15 us
         # "ru",  # 15 us
@@ -95,7 +95,7 @@ def cur_role(request):
         # "zh",  # 12 us
         # "fr",  # 11 us
         # "pl",  # 10 us
-        # "ro",  # 10 us
+        "ro",  # 10 us
         # "ar",  # 8 us
         # "nl",  # 8 us
         # "el",  # 5 us
@@ -125,10 +125,10 @@ def cur_language(request):
 @pytest.fixture(
     scope="class",
     params=[
-        # "gb",  # United Kingdom - "FCA"
+        "gb",  # United Kingdom - "FCA"
         # "de",  # Germany - "CYSEC"
         # "au",  # Australia - "ASIC"
-        "ae",  # United Arab Emirates - "SCB"
+        # "ae",  # United Arab Emirates - "SCB"
 
         # "gr",  # Greece - "CYSEC"
         # "es",  # Spain - "CYSEC"
