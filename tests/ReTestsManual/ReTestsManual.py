b1f43f2e92d2e07018b6e74ede24a62b7d15b9b0
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-15 11:56:44 +0300

    rf ReTestsManual

--- tests/ReTestsManual/ReTestsManual.py
+++ tests/ReTestsManual/ReTestsManual.py
@@ -117,86 +117,6 @@ class TestManualBugs:
             attachment_type=AttachmentType.PNG,
         )
 
-    @pytest.mark.parametrize('cur_language', [''])
-    @pytest.mark.parametrize('cur_country', ['gb'])
-    @pytest.mark.parametrize('cur_role', ['NoReg'])
-    @allure.step("Bug#48: 404 status code is displayed on the [USD/JPY-Rate] page and switching to an ASIC license")
-    @allure.severity(allure.severity_level.MINOR)
-    @pytest.mark.test_48
-    # @pytest.mark.skip(reason="Skipped for debugging")
-    def test_48(
-            self, worker_id, d, cur_login, cur_password, cur_role, cur_language, cur_country):
-        """
-        404 status code is displayed on the "USD/JPY-Rate" page and switching to an ASIC license after
-        clicking the "USD/JPY" trading instrument in the "Forex Market" Widget in the "Forex" page
-        (Floating bug, also open another tab in parallel with another licence(Try all three roles) )
-            1. Hover over the [Markets] menu section
-            2. Click the [Forex] menu item
-            3. Scroll down to the "Forex Market" widget
-            4. Click the "USD/JPY"  trading instrument
-        """
-
-        build_dynamic_arg_v4(
-            d, worker_id, cur_language, cur_country, cur_role,
-            "Bugs_26012024_CCW_WEB", "Capital.com FCA",
-            ".48", "404 status code is displayed on the [USD/JPY-Rate] page and switching to an ASIC license")
-
-        page_conditions = NewConditions(d, "")
-        link = page_conditions.preconditions(
-            d, CapitalComPageSrc.URL_NEW, "", cur_language, cur_country, cur_role, cur_login, cur_password)
-
-        menu = MainMenu(d, link)
-        menu.open_markets_forex_sub_menu(d, cur_language, cur_country, link)
-
-        # определение количества страниц
-        markets_page = MenuSections(d)
-        pagination = markets_page.elements_are_located(markets_page.MARKETS_PAGINATION_LIST)
-        qty_pages = int(pagination[-2].text)
-        # qty_pages = 1
-        print("qty_pages=", qty_pages)
-
-        # перебор страниц
-        error_trade_instrument_list = []
-        for i in range(qty_pages):
-            print("page:", i + 1)
-            most_traded_list = markets_page.elements_are_located(markets_page.MARKETS_MOST_TRADE_LINK_LIST)
-            for j in range(len(most_traded_list)):
-                most_traded_list = markets_page.elements_are_located(markets_page.MARKETS_MOST_TRADE_LINK_LIST)
-
-                markets_page.go_to_element(most_traded_list[j])
-                most_traded_instrument_name = most_traded_list[j].text
-                try:
-                    markets_page.element_is_clickable(most_traded_list[j]).click()
-                except StaleElementReferenceException:
-                    print("StaleElementReferenceException")
-                    most_traded_list = markets_page.elements_are_located(
-                        markets_page.MARKETS_MOST_TRADE_LINK_LIST)
-                    markets_page.element_is_clickable(most_traded_list[j]).click()
-
-                err_404 = markets_page.elements_are_located(markets_page.MARKETS_MOST_TRADE_INSTRUMENT_404, 1)
-                if err_404:
-                    error_trade_instrument_list.append(most_traded_instrument_name + ":404")
-                    print("error_404: ", error_trade_instrument_list)
-                    assert False, (
-                        f"Bug#48. Expected Result: Page of the corresponding trading instrument"
-                        f"{error_trade_instrument_list} is opened. \n"
-                        f"Actual Result: 404 status code is displayed on the {error_trade_instrument_list} "
-                        f"page and switching to an ASIC license . \n"
-                        f"error_404: {error_trade_instrument_list}. \n"
-                        f"qty_pages: {qty_pages}")
-                else:
-                    d.back()
-
-            pagination = markets_page.elements_are_located(markets_page.MARKETS_PAGINATION_LIST)
-            if i != qty_pages - 1:
-                pagination[-1].click()
-            time.sleep(1)
-            print("Non one 404 error")
-        allure.attach(
-            d.get_screenshot_as_png(),
-            name=f"Screenshot{datetime.now()}",
-            attachment_type=AttachmentType.PNG,
-        )
 
     @pytest.mark.parametrize('cur_language', [''])
     @pytest.mark.parametrize('cur_country', ['gb'])
@@ -1516,6 +1436,87 @@ class TestManualBugs:
             'Bug#44. Validation error is cleared'
             '\n'
             'Actual result: Validation error is not cleared')
+        allure.attach(
+            d.get_screenshot_as_png(),
+            name=f"Screenshot{datetime.now()}",
+            attachment_type=AttachmentType.PNG,
+        )
+
+    @pytest.mark.parametrize('cur_language', [''])
+    @pytest.mark.parametrize('cur_country', ['gb'])
+    @pytest.mark.parametrize('cur_role', ['NoReg'])
+    @allure.step("Bug#48: 404 status code is displayed on the [USD/JPY-Rate] page and switching to an ASIC license")
+    @allure.severity(allure.severity_level.MINOR)
+    @pytest.mark.test_48
+    # @pytest.mark.skip(reason="Skipped for debugging")
+    def test_48(
+            self, worker_id, d, cur_login, cur_password, cur_role, cur_language, cur_country):
+        """
+        404 status code is displayed on the "USD/JPY-Rate" page and switching to an ASIC license after
+        clicking the "USD/JPY" trading instrument in the "Forex Market" Widget in the "Forex" page
+        (Floating bug, also open another tab in parallel with another licence(Try all three roles) )
+            1. Hover over the [Markets] menu section
+            2. Click the [Forex] menu item
+            3. Scroll down to the "Forex Market" widget
+            4. Click the "USD/JPY"  trading instrument
+        """
+
+        build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "Bugs_26012024_CCW_WEB", "Capital.com FCA",
+            ".48", "404 status code is displayed on the [USD/JPY-Rate] page and switching to an ASIC license")
+
+        page_conditions = NewConditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL_NEW, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        menu = MainMenu(d, link)
+        menu.open_markets_forex_sub_menu(d, cur_language, cur_country, link)
+
+        # определение количества страниц
+        markets_page = MenuSections(d)
+        pagination = markets_page.elements_are_located(markets_page.MARKETS_PAGINATION_LIST)
+        qty_pages = int(pagination[-2].text)
+        # qty_pages = 1
+        print("qty_pages=", qty_pages)
+
+        # перебор страниц
+        error_trade_instrument_list = []
+        for i in range(qty_pages):
+            print("page:", i + 1)
+            most_traded_list = markets_page.elements_are_located(markets_page.MARKETS_MOST_TRADE_LINK_LIST)
+            for j in range(len(most_traded_list)):
+                most_traded_list = markets_page.elements_are_located(markets_page.MARKETS_MOST_TRADE_LINK_LIST)
+
+                markets_page.go_to_element(most_traded_list[j])
+                most_traded_instrument_name = most_traded_list[j].text
+                try:
+                    markets_page.element_is_clickable(most_traded_list[j]).click()
+                except StaleElementReferenceException:
+                    print("StaleElementReferenceException")
+                    most_traded_list = markets_page.elements_are_located(
+                        markets_page.MARKETS_MOST_TRADE_LINK_LIST)
+                    markets_page.element_is_clickable(most_traded_list[j]).click()
+
+                err_404 = markets_page.elements_are_located(markets_page.MARKETS_MOST_TRADE_INSTRUMENT_404, 1)
+                if err_404:
+                    error_trade_instrument_list.append(most_traded_instrument_name + ":404")
+                    print("error_404: ", error_trade_instrument_list)
+                    assert False, (
+                        f"Bug#48. Expected Result: Page of the corresponding trading instrument"
+                        f"{error_trade_instrument_list} is opened. \n"
+                        f"Actual Result: 404 status code is displayed on the {error_trade_instrument_list} "
+                        f"page and switching to an ASIC license . \n"
+                        f"error_404: {error_trade_instrument_list}. \n"
+                        f"qty_pages: {qty_pages}")
+                else:
+                    d.back()
+
+            pagination = markets_page.elements_are_located(markets_page.MARKETS_PAGINATION_LIST)
+            if i != qty_pages - 1:
+                pagination[-1].click()
+            time.sleep(1)
+            print("Non one 404 error")
         allure.attach(
             d.get_screenshot_as_png(),
             name=f"Screenshot{datetime.now()}",
