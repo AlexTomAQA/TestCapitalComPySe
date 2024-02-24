36345673180c66959ac37bfdf763c965a04ed9dd
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-24 05:35:16 +0300

    fix us_11.00.04

--- tests/US_11_Education/US_11-00-04_Trading_strategies/US_11-00-04_Trading_strategies.py
+++ tests/US_11_Education/US_11-00-04_Trading_strategies/US_11-00-04_Trading_strategies.py
@@ -34,7 +34,7 @@ class TradingStrategies:
             ".00_01", "Testing button [Create account] on the Main banner")
 
         Common().check_language_in_list_and_skip_if_not_present(cur_language, [""])
-        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         page_conditions = NewConditions(d, "")
         link = page_conditions.preconditions(d, CapitalComPageSrc.URL_NEW, "", cur_language, cur_country,
@@ -58,7 +58,7 @@ class TradingStrategies:
         bid = build_dynamic_arg_v4(
             d, worker_id, cur_language, cur_country, cur_role,
             "11.00.04", "Learn to trade > Menu item [Trading Strategies]",
-            ".00.02", "Testing button [Try demo account] on the Main banner")
+            ".00_02", "Testing button [Try demo account] on the Main banner")
 
         Common().check_language_in_list_and_skip_if_not_present(cur_language, [""])
         Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
