3d58877dec52ecb11fc7e1e7a5c163b1eda50b0c
MikeTaran <mtaran.job@gmail.com>
2024-02-26 18:41:05 +0200

    merge

--- conftest.py
+++ conftest.py
@@ -94,7 +94,7 @@ def cur_role(request):
         "de",  # 15 us
         # "it",  # 15 us
         # "ru",  # 15 us
-        "cn",  # 13 us Education to trade present, financial glossary not present
+        # "cn",  # 13 us Education to trade present, financial glossary not present
         # "zh",  # 12 us
         # "fr",  # 11 us
         # "pl",  # 10 us
