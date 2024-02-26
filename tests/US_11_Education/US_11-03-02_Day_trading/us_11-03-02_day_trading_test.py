b9ce88453d1941d7dfb2dad1917606dbe3746b39
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-26 16:08:02 +0300

    fix Bug 'Education menu not present for not En language for FCA license for us_11.03.02

--- tests/US_11_Education/US_11-03-02_Day_trading/us_11-03-02_day_trading_test.py
+++ tests/US_11_Education/US_11-03-02_Day_trading/us_11-03-02_day_trading_test.py
@@ -54,6 +54,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_01", "Testing button [Start Trading] on Main banner")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
 
@@ -75,6 +76,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_02", "Testing button [Try demo] on Main banner")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
 
@@ -95,6 +97,7 @@ class TestDayTrading:
                                    "11.03.02", "Education > Menu item [Day Trading]",
                                    ".00_03", "Testing button [Trade] in Most traded block")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         if cur_country == 'gb':
@@ -118,6 +121,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_04", "Testing button [Start trading] in Content block")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         if cur_country == 'gb':
@@ -141,6 +145,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_05", "Testing button [Practise for free] in Content block")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         if cur_country == 'gb':
@@ -176,6 +181,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_06", "Test button [Download on the App Store] in Block \"Sign up and trade smart today!\"")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
 
@@ -203,6 +209,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_07", "Test button [Get it on Google Play] in Block \"Sign up and trade smart today!\"")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
 
         menu_link = self.us_link.get_us_link(d, cur_language, cur_country, cur_role, cur_login, cur_password)
@@ -229,6 +236,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_08", "Testing button [Explore Web Platform] in Block \"Sign up and trade smart today!\"")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
 
         menu_link = self.us_link.get_us_link(d, cur_language, cur_country, cur_role, cur_login, cur_password)
@@ -261,6 +269,7 @@ class TestDayTrading:
             "11.03.02", "Education > Menu item [Day Trading]",
             ".00_09", "Testing button [1. Create & verify your account] in Block 'Steps trading'")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
 
         cur_page_url = self.us_link.get_us_link(d, cur_language, cur_country, cur_role, cur_login, cur_password)
