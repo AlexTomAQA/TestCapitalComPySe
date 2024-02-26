c3c452b2812a89aee2034f0dfd903ebc8205ce40
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-26 23:01:16 +0300

    set ar/fca/all

--- conftest.py
+++ conftest.py
@@ -92,12 +92,12 @@ def cur_role(request):
         # "it",  # 15 us
         # "hu",  # 5 us Magyar
         # "ru",  # 15 us
-        "cn",  # 13 us Education to trade present, financial glossary not present
+        # "cn",  # 13 us Education to trade present, financial glossary not present
         # "zh",  # 12 us
         # "fr",  # 11 us
         # "pl",  # 10 us
         # "ro",  # 10 us
-        # "ar",  # 8 us
+        "ar",  # 8 us
         # "nl",  # 8 us
         # "el",  # 5 us
     ],
