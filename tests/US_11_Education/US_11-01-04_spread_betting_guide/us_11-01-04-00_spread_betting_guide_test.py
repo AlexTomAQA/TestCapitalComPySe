50a56cf3d6ef076bd160c0f6714cb4dc10993b91
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-02-26 19:09:12 +0300

    fix Bug 'Education menu not present for not En language for FCA license for us_11.01.04

--- tests/US_11_Education/US_11-01-04_spread_betting_guide/us_11-01-04-00_spread_betting_guide_test.py
+++ tests/US_11_Education/US_11-01-04_spread_betting_guide/us_11-01-04-00_spread_betting_guide_test.py
@@ -41,9 +41,10 @@ class TestSpreadBettingGuidePretest:
             "11.01.04", "Education > Menu item [Spread betting guide]",
             ".00_01", "Testing button [Start Trading] on Main banner")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().check_language_in_list_and_skip_if_not_present(cur_language, ["", "es", "cn", "ru"])
-        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
+        # Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         page_conditions = Conditions(d, "")
         main_page_link = page_conditions.preconditions(
@@ -68,9 +69,10 @@ class TestSpreadBettingGuidePretest:
             "11.01.04", "Education > Menu item [Spread betting guide]",
             ".00_02", "Testing button [Try demo] on Main banner")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().check_language_in_list_and_skip_if_not_present(cur_language, ["", "es", "cn", "ru"])
-        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
+        # Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         page_conditions = Conditions(d, "")
         main_page_link = page_conditions.preconditions(
@@ -95,9 +97,10 @@ class TestSpreadBettingGuidePretest:
             "11.01.04", "Education > Menu item [Spread betting guide]",
             ".00_03", "Testing button [Trade] in Most traded block")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().check_language_in_list_and_skip_if_not_present(cur_language, ["", "es", "cn", "ru"])
-        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
+        # Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         page_conditions = Conditions(d, "")
         main_page_link = page_conditions.preconditions(
@@ -122,9 +125,10 @@ class TestSpreadBettingGuidePretest:
             "11.01.04", "Education > Menu item [Spread betting guide]",
             ".00_04", "Testing button [Create your account] in block [Steps trading]")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().check_language_in_list_and_skip_if_not_present(cur_language, ["", "es", "cn", "ru"])
-        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
+        # Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         page_conditions = Conditions(d, "")
         main_page_link = page_conditions.preconditions(
@@ -149,9 +153,10 @@ class TestSpreadBettingGuidePretest:
             "11.01.04", "Education > Menu item [Spread betting guide]",
             ".00_05", "Testing button [Start trading] in article")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().check_language_in_list_and_skip_if_not_present(cur_language, ["", "es", "cn", "ru"])
-        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
+        # Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         page_conditions = Conditions(d, "")
         main_page_link = page_conditions.preconditions(
@@ -176,9 +181,10 @@ class TestSpreadBettingGuidePretest:
             "11.01.04", "Education > Menu item [Spread betting guide]",
             ".00_06", "Testing button [Create account] in article")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().check_language_in_list_and_skip_if_not_present(cur_language, [""])
-        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
+        # Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         page_conditions = Conditions(d, "")
         main_page_link = page_conditions.preconditions(
@@ -201,9 +207,10 @@ class TestSpreadBettingGuidePretest:
             "11.01.04", "Education > Menu item [Spread betting guide]",
             ".00_99", "Pretest for US_11.01.04.01")
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
         Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
         Common().check_language_in_list_and_skip_if_not_present(cur_language, [""])
-        Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
+        # Common().check_country_in_list_and_skip_if_not_present(cur_country, ["gb"])
 
         if count == 0:
             pytest.skip('The list of "Spread betting guide" links is already created')
