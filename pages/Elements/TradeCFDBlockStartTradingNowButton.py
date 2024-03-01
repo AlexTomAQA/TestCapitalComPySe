c0696ebcba00712772cf007e95a90f2ec041b871
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-03-01 22:07:13 +0300

    fix  pages/Elements/TradeCFDBlockStartTradingNowButton.py

--- pages/Elements/TradeCFDBlockStartTradingNowButton.py
+++ pages/Elements/TradeCFDBlockStartTradingNowButton.py
@@ -1,122 +1,97 @@
-# from datetime import datetime
-# import allure
-# import pytest
-# from pages.Signup_login.signup_login import SignupLogin
-# from pages.base_page import BasePage
-# from pages.Elements.AssertClass import AssertClass
-# from pages.Markets.markets_locators import BlockTradeCFD
-# from selenium.common.exceptions import ElementClickInterceptedException
-#
-#
-# class TradeCFDBlockStartTradingNow(BasePage):
-#
-#     @allure.step(
-#         f"{datetime.now()}   Start Full test for Start trading button of Trade CFD Block"
-#     )
-#     def full_test_with_tpi(self, d, cur_language, cur_country, cur_role, cur_item_link):
-#
-#         self.arrange_(d, cur_item_link, True)
-#         self.element_click()
-#
-#         test_element = AssertClass(d, cur_item_link, self.bid)
-#         match cur_role:
-#             case "NoReg":
-#                 test_element.assert_signup(d, cur_language, cur_item_link)
-#             case "NoAuth":
-#                 test_element.assert_login(d, cur_language, cur_item_link)
-#             case "Auth":
-#                 test_element.assert_trading_platform_v4(d, cur_item_link)
-#
-#     def full_test(self, d, cur_language, cur_country, cur_role, page_url):
-#
-#         self.arrange_(d, page_url)
-#
-#         self.element_click()
-#
-#         test_element = AssertClass(d, page_url)
-#         match cur_role:
-#             case "NoReg":
-#                 test_element.assert_signup(d, cur_language, page_url)
-#             case "NoAuth":
-#                 test_element.assert_login(d, cur_language, page_url)
-#             case "Auth":
-#                 test_element.assert_trading_platform_v4(d, page_url)
-#
-#     def arrange_(self, d, cur_item_link, always=False):
-#         print(f"\n{datetime.now()}   1. Arrange_v0")
-#
-#         if not self.current_page_is(cur_item_link):
-#             self.link = cur_item_link
-#             self.open_page()
-#
-#         button_list = self.driver.find_elements(*BlockTradeCFD.START_TRADING_NOW_BUTTON)
-#         if len(button_list) == 0:
-#             print(
-#                 f"{datetime.now()}   => BUTTON_START_TRADING_NOW is not present on the page!"
-#             )
-#             del button_list
-#             msg = "Testing element 'BUTTON_START_TRADING_NOW in the Trade CFD Block' is not on this page"
-#             if always:
-#                 pytest.fail(f"Bug № ??? {msg}")
-#             else:
-#                 pytest.skip(msg)
-#
-#         print(f"{datetime.now()}   BUTTON_START_TRADING_NOW scroll =>")
-#         self.driver.execute_script(
-#             'return arguments[0].scrollIntoView({block: "center", inline: "nearest"});',
-#             button_list[0],
-#         )
-#
-#         print(f"{datetime.now()}   BUTTON_START_TRADING_NOW is visible? =>")
-#         if self.element_is_visible(BlockTradeCFD.START_TRADING_NOW_BUTTON):
-#             print(f"{datetime.now()}   => BUTTON_START_TRADING is visible on the page!")
-#         else:
-#             print(
-#                 f"{datetime.now()}   => BUTTON_START_TRADING_NOW is not visible on the page!"
-#             )
-#             pytest.fail(
-#                 "Bug! Testing element 'BUTTON_START_TRADING_NOW in Trade CFD Block' is present on this page, "
-#                 "but not visible"
-#             )
-#
-#     @allure.step("Click button [Start Trading Now] on Trade CFD Block")
-#     def element_click(self):
-#         print(f"\n{datetime.now()}   2. Act_v0")
-#         print(f"{datetime.now()}   Start Click button [Start Trading Now] =>")
-#
-#         button_list = self.driver.find_elements(*BlockTradeCFD.START_TRADING_NOW_BUTTON)
-#         print(f"{datetime.now()}   BUTTON_START_TRADING_NOW is clickable? =>")
-#         time_out = 3
-#         if not self.element_is_clickable(button_list[0], time_out):
-#             print(
-#                 f"{datetime.now()}   => BUTTON_START_TRADING_NOW is not clickable after {time_out} sec. Stop TC>"
-#             )
-#             pytest.fail(
-#                 f"BUTTON_START_TRADING_NOW is not clickable after {time_out} sec."
-#             )
-#
-#         print(f"{datetime.now()}   BUTTON_START_TRADING_NOW click =>")
-#         try:
-#             button_list[0].click()
-#             print(f"{datetime.now()}   => BUTTON_START_TRADING_NOW clicked")
-#         except ElementClickInterceptedException:
-#             print(f"{datetime.now()}   => BUTTON_START_TRADING_NOW not clicked")
-#             print(
-#                 f"{datetime.now()}   'Signup' or 'Login' form is automatically opened"
-#             )
-#
-#             page_ = SignupLogin(self.driver)
-#             if page_.close_signup_form():
-#                 pass
-#             elif page_.close_login_form():
-#                 pass
-#             elif page_.close_signup_page():
-#                 pass
-#             else:
-#                 page_.close_login_page()
-#
-#             del page_
-#             button_list[0].click()
-#
-#         del button_list
-#         return True
+"""
+-*- coding: utf-8 -*-
+@Time    : 2024/02/27 23:00
+@Author  : podchasova11
+"""
+from datetime import datetime
+import pytest
+import allure
+from pages.Signup_login.signup_login import SignupLogin
+from pages.base_page import BasePage
+from pages.Elements.AssertClass import AssertClass
+from pages.Elements.testing_elements_locators import TradingInstrumentsBlockLocators
+from selenium.common.exceptions import ElementClickInterceptedException
+
+
+class TradeCFDBlockStartTradingNowButton(BasePage):
+
+    @allure.step(f'{datetime.now()}   Start Full test for Start trading now button of Block Trading Instruments')
+    def full_test_with_tpi(self, d, cur_language, cur_country, cur_role, cur_item_link):
+
+        self.arrange_(d, cur_item_link, True)
+        self.element_click()
+
+        test_element = AssertClass(d, cur_item_link, self.bid)
+        match cur_role:
+            case "NoReg":
+                test_element.assert_signup(d, cur_language, cur_item_link)
+            case "NoAuth":
+                test_element.assert_login(d, cur_language, cur_item_link)
+            case "Auth":
+                test_element.assert_trading_platform_v4(d, cur_item_link)
+
+    def arrange_(self, d, cur_item_link, always=False):
+        print(f"\n{datetime.now()}   1. Arrange_v0")
+
+        if not self.current_page_is(cur_item_link):
+            self.link = cur_item_link
+            self.open_page()
+
+        button_list = self.driver.find_elements(*TradingInstrumentsBlockLocators.BUTTON_START_TRADING_NOW)
+        if len(button_list) == 0:
+            print(f"{datetime.now()}   => BUTTON_START_TRADING_NOW is not present on the page!")
+            del button_list
+            msg = "Testing element 'BUTTON_START_TRADING_NOW on the Block Trading Instruments' is not on this page"
+            if always:
+                pytest.fail(f"Bug № ??? {msg}")
+            else:
+                pytest.skip(msg)
+
+        print(f"{datetime.now()}   BUTTON_START_TRADING_NOW scroll =>")
+        self.driver.execute_script(
+            'return arguments[0].scrollIntoView({block: "center", inline: "nearest"});', button_list[0]
+        )
+
+        print(f"{datetime.now()}   BUTTON_START_TRADING_NOW is visible? =>")
+        if self.element_is_visible(TradingInstrumentsBlockLocators.BUTTON_START_TRADING_NOW):
+            print(f"{datetime.now()}   => BUTTON_START_TRADING is visible on the page!")
+        else:
+            print(f"{datetime.now()}   => BUTTON_START_TRADING is not visible on the page!")
+            pytest.fail("Bug! Testing element 'BUTTON_START_TRADING on Block Trading Instruments' present on this page,"
+                        "but not visible")
+
+    @allure.step("Click button [Start Trading Now] on Block Trading Instruments")
+    def element_click(self):
+        print(f"\n{datetime.now()}   2. Act_v0")
+        print(f"{datetime.now()}   Start Click button [Start Trading Now] =>")
+
+        button_list = self.driver.find_elements(*TradingInstrumentsBlockLocators.BUTTON_START_TRADING_NOW)
+        print(f"{datetime.now()}   BUTTON_START_TRADING_NOW is clickable? =>")
+        time_out = 3
+        if not self.element_is_clickable(button_list[0], time_out):
+            print(f"{datetime.now()}   => BUTTON_START_TRADING_NOW is not clickable after {time_out} sec. Stop TC>")
+            pytest.fail(f"BUTTON_START_TRADING_NOW is not clickable after {time_out} sec.")
+
+        print(f"{datetime.now()}   BUTTON_START_TRADING_NOW click =>")
+        try:
+            button_list[0].click()
+            print(f"{datetime.now()}   => BUTTON_START_TRADING_NOW clicked")
+        except ElementClickInterceptedException:
+            print(f"{datetime.now()}   => BUTTON_START_TRADING_NOW not clicked")
+            print(f"{datetime.now()}   'Signup' or 'Login' form is automatically opened")
+
+            page_ = SignupLogin(self.driver)
+            if page_.close_signup_form():
+                pass
+            elif page_.close_login_form():
+                pass
+            elif page_.close_signup_page():
+                pass
+            else:
+                page_.close_login_page()
+
+            del page_
+            button_list[0].click()
+
+        del button_list
+        return True
