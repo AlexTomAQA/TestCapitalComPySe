d40a79f481633cdb274c05d18fc8459a5e04806e
Ivan <ivkedrov@gmail.com>
2024-03-05 21:29:24 +0300

    Added test_002_block_step_trading_create_verify_your_account

--- tests/US_01_Markets/US_01-05_Commodities/US_01-05-00_Commodities_test.py
+++ tests/US_01_Markets/US_01-05_Commodities/US_01-05-00_Commodities_test.py
@@ -1,6 +1,7 @@
 import allure
 import pytest
 
+from pages.Elements.StepTradingBlock import BlockStepTrading
 from pages.common import Common
 from tests.build_dynamic_arg import build_dynamic_arg_v4
 from pages.Menu.menu import MenuSection
@@ -40,4 +41,31 @@ class TestCommodities:
         cur_page_url = page_menu.sub_menu_commodities_move_focus_click(d, cur_language)
 
         test_element = TradeCFDBlockStartTradingNowButton(d, cur_page_url, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_url)
+
+    @allure.step("Start test of button [1. Create & verify your account] in Step trading block")
+    @pytest.mark.test_002
+    def test_002_block_step_trading_create_verify_your_account(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+        Check: Button [1. Create & verify account]
+        Language: All. License: All, except FCA. Role: All.
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.05", "Markets > Menu Item [Commodities]",
+            ".00_004", "Testing button [1. Create your account] in Step trading block")
+
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
+        cur_page_url = page_menu.sub_menu_commodities_move_focus_click(d, cur_language)
+
+        test_element = BlockStepTrading(d, cur_page_url, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_url)
