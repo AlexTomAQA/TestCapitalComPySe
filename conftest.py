91985f8ba651c5248a00d16123c38315e475e294
Aleksandr Tomelo <146713192+AlexTomAQA@users.noreply.github.com>
2024-03-06 10:33:06 +0300

    set ar/gb/all

--- conftest.py
+++ conftest.py
@@ -95,14 +95,14 @@ def cur_role(request):
         # "",  # "en" - 21 us
         # "es",  # 20 us
         # "de",  # 15 us
-        "it",  # 15 us
+        # "it",  # 15 us
         # "ru",  # 15 us
         # "cn",  # 13 us Education to trade present, financial glossary not present
         # "zh",  # 12 us
         # "fr",  # 11 us
         # "pl",  # 10 us
         # "ro",  # 10 us
-        # "ar",  # 8 us
+        "ar",  # 8 us
         # "nl",  # 8 us
         # "el",  # 5 us
         # "hu",  # 5 us Magyar
@@ -128,8 +128,8 @@ def cur_language(request):
     params=[
         # "ae",  # United Arab Emirates - "SCB"
         # "au",  # Australia - "ASIC"
-        "de",  # Germany  - "CYSEC"
-        # "gb",  # United Kingdom - "FCA"
+        # "de",  # Germany  - "CYSEC"
+        "gb",  # United Kingdom - "FCA"
         #
         # "gr",  # Greece - "CYSEC"
         # "es",  # Spain - "CYSEC"
