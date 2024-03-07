d1d2d40a757d8ed7534ffb22962538c710ab5648
last_name <artemdashkov@gmail.com>
2024-03-07 22:43:16 +0300

    added test_105: testing [Sell] button in Our Markets block

--- tests/US_00_Main_page/US_00-00_Main_page/us_00-00_main_page_test.py
+++ tests/US_00_Main_page/US_00-00_Main_page/us_00-00_main_page_test.py
@@ -12,6 +12,7 @@ from pages.Elements.ForLearnerTradersBlockTryDemoButton import ForLearnerTraders
 from pages.Elements.ForLearnerTradersBlockSignUpButton import ForLearnerTradersBlockSignUpButton
 from pages.Elements.MainBannerSignUpButtonMainPage import MainBannerSignUpButtonMainPage
 from pages.Elements.MainBannerTryDemoButtonMainPage import MainBannerTryDemoButtonMainPage
+from pages.Elements.OurMarketsTableSellButton import SellButtonOurMarketsTable
 from pages.Elements.StepTradingBlock import BlockStepTrading
 from pages.Elements.WhyChooseBlockTryDemoButton import WhyChooseBlockTryDemoButton
 from pages.Elements.WhyChooseBlockSignUpButton import WhyChooseBlockSignUpButton
@@ -119,6 +120,31 @@ class TestMainPage:
 
         test_element = WhyChooseBlockSignUpButton(d, main_page_link, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, main_page_link)
+
+    @allure.step("Start test of button [Sell] in Block 'Our markets'")
+    @pytest.mark.test_105
+    def test_105_our_markets_block_sell_button(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, market, instrument):
+        """
+        Check: Button [Sell] in Block 'Our markets' Main Page
+        Language: EN. License: FCA.
+        """
+        test_title = ("00", "Main Page",
+                      ".00_105",
+                      f"Testing button [Sell] in Block 'Our markets' {market} market, {instrument} instrument")
+
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role, *test_title)
+
+        Common().check_language_in_list_and_skip_if_not_present(cur_language, [''])
+        Common().check_country_in_list_and_skip_if_not_present(cur_country, ['gb'])
+
+        page_conditions = NewConditions(d, "")
+        main_page_link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL_NEW, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        test_element = SellButtonOurMarketsTable(d, main_page_link, bid)
+        test_element.full_test(d, cur_language, cur_country, cur_role, main_page_link, market, instrument)
 
     @allure.step("Start test of button [Try demo] in Block 'For learner traders'")
     @pytest.mark.test_107
