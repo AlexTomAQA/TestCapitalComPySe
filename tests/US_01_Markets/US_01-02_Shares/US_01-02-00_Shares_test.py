b48a9eda843ebf6606a757096352d4416e8c7dce
Amid-Kirdum <mudrik1200@gmail.com>
2024-03-09 22:28:39 +0300

    add AT_004 > button [1.Create & verify your account]

--- tests/US_01_Markets/US_01-02_Shares/US_01-02-00_Shares_test.py
+++ tests/US_01_Markets/US_01-02_Shares/US_01-02-00_Shares_test.py
@@ -12,6 +12,7 @@ from pages.conditions import Conditions
 from src.src import CapitalComPageSrc
 from pages.Menu.menu import MenuSection
 from pages.Elements.TradeCFDBlockStartTradingNowButton import TradeCFDBlockStartTradingNowButton
+from pages.Elements.StepTradingBlock import BlockStepTrading
 
 count = 1
 
@@ -44,9 +45,35 @@ class TestShares:
         page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
         cur_page_link = page_menu.sub_menu_shares_move_focus_click(d, cur_language)
 
-        test_element = TradeCFDBlockStartTradingNowButton(d,cur_page_link, bid)
+        test_element = TradeCFDBlockStartTradingNowButton(d, cur_page_link, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link)
 
+    @allure.step("Test button [1.Create & verify your account] in Block 'Still looking for a broker you can trust?'")
+    @pytest.mark.test_004
+    def test_004_create_your_account(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+        Check: Button [1.Create & verify your account] in block "Still looking for a broker you can trust?"
+        Language: ALL.
+        License: All,except FCA (GB country)
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.02", "Markets > Menu item [Shares]",
+            ".00_004", "Testing button [1.Create & verify your account] "
+                       "on Block 'Still looking for a broker you can trust?'")
 
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+        Common().check_country_in_list_and_skip_if_present(cur_country, ['gb'])
 
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
+        cur_page_link = page_menu.sub_menu_shares_move_focus_click(d, cur_language)
+
+        test_element = BlockStepTrading(d, cur_page_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link)
 
