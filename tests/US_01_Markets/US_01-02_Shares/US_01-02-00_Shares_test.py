7b9bde60063ec08c651ee6988dd82eebb5561f2f
Amid-Kirdum <mudrik1200@gmail.com>
2024-03-09 16:25:30 +0300

    add AT_001 > button [ Start Trading Now]

---
+++ tests/US_01_Markets/US_01-02_Shares/US_01-02-00_Shares_test.py
@@ -0,0 +1,52 @@
+"""
+-*- coding: utf-8 -*-
+@Time    : 2024/03/03 23:12 GMT+3
+@Author  : Dmitry Mudrik
+"""
+import pytest
+import allure
+
+from tests.build_dynamic_arg import build_dynamic_arg_v4
+from pages.common import Common
+from pages.conditions import Conditions
+from src.src import CapitalComPageSrc
+from pages.Menu.menu import MenuSection
+from pages.Elements.TradeCFDBlockStartTradingNowButton import TradeCFDBlockStartTradingNowButton
+
+count = 1
+
+@pytest.mark.us_01_02
+class TestShares:
+    page_conditions = None
+
+    @allure.step("Start test button [Start Trading Now] in Block 'Trade Share CFDs'")
+    @pytest.mark.test_001
+    def test_001_block_trade_share_CFDs_start_trading_now_button(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+        Check: Button [Start Trading Now]
+        Language: All. License: All,except FCA (GB country)
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.04","Markets > Menu item [Shares]",
+            ".00_001", "Testing button [Start Trading Now] on Block 'Trade Share CFDs'")
+
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+        Common().check_country_in_list_and_skip_if_not_present(cur_country,['gb'])
+        # Common().check_language_in_list_and_skip_if_not_present(cur_language, [])
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
+        cur_page_link = page_menu.sub_menu_shares_move_focus_click(d, cur_language)
+
+        test_element = TradeCFDBlockStartTradingNowButton(d,cur_page_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link)
+
+
+
+
