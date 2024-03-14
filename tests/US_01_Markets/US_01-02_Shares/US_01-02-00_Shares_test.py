ac0bb98900cd59ddc6095267dbde927aee1218e2
Amid-Kirdum <mudrik1200@gmail.com>
2024-03-15 00:06:37 +0300

    add AT_003 > button [Buy]

--- tests/US_01_Markets/US_01-02_Shares/US_01-02-00_Shares_test.py
+++ tests/US_01_Markets/US_01-02_Shares/US_01-02-00_Shares_test.py
@@ -7,6 +7,7 @@ import allure
 import pytest
 
 from pages.Elements.StepTradingBlock import BlockStepTrading
+from pages.Elements.TableTradingInstrumentsBuyButton import TableTradingInstrumentsBuyButton
 from pages.Elements.TableTradingInstrumentsSellButton import TableTradingInstrumentsSellButton
 from pages.Elements.TradeCFDBlockStartTradingNowButton import TradeCFDBlockStartTradingNowButton
 from pages.Menu.menu import MenuSection
@@ -47,13 +48,13 @@ class TestShares:
         page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
         cur_page_link = page_menu.sub_menu_shares_move_focus_click(d, cur_language)
 
-        test_element = TableTradingInstrumentsSellButton(d, cur_page_link, bid)
+        test_element = TradeCFDBlockStartTradingNowButton(d, cur_page_link, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link)
 
     @allure.step("Test button [Sell] 'numeric values' in Widget 'Trading instrument'")
     @pytest.mark.test_002
     def test_002_sell_widget_trading_instrument(
-            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_sort):
         """
         Check: Button [Sell]'numeric values' in Widget 'Trading instrument'
         Language: All. License: All,except FCA (GB country)
@@ -74,8 +75,35 @@ class TestShares:
         page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
         cur_page_link = page_menu.sub_menu_shares_move_focus_click(d, cur_language)
 
-        test_element = TradeCFDBlockStartTradingNowButton(d, cur_page_link, bid)
-        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link)
+        test_element = TableTradingInstrumentsSellButton(d, cur_page_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link, cur_sort)
+
+    @allure.step("Test button [Buy] 'numeric values' in Widget 'Trading instrument'")
+    @pytest.mark.test_003
+    def test_003_buy_widget_trading_instrument(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_sort):
+        """
+        Check: Button [Buy]'numeric values' in Widget 'Trading instrument'
+        Language: All. License: All,except FCA (GB country)
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.04", "Markets > Menu item [Shares]",
+            ".00_003", "Testing button [Buy] 'numeric values' in Widget 'Trading instrument'")
+
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
+        cur_page_link = page_menu.sub_menu_shares_move_focus_click(d, cur_language)
+
+        test_element = TableTradingInstrumentsBuyButton(d, cur_page_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link, cur_sort)
 
     @allure.step("Test button [1.Create & verify your account] in Block 'Still looking for a broker you can trust?'")
     @pytest.mark.test_004
