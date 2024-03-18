cb46ae833d906724654d3051ec68fae431075d58
Ivan <ivkedrov@gmail.com>
2024-03-18 15:52:20 +0300

    Added test_002 and test_003

--- tests/US_01_Markets/US_01-05_Commodities/US_01-05-00_Commodities_test.py
+++ tests/US_01_Markets/US_01-05_Commodities/US_01-05-00_Commodities_test.py
@@ -2,6 +2,8 @@ import allure
 import pytest
 
 from pages.Elements.StepTradingBlock import BlockStepTrading
+from pages.Elements.TableTradingInstrumentsBuyButton import TableTradingInstrumentsBuyButton
+from pages.Elements.TableTradingInstrumentsSellButton import TableTradingInstrumentsSellButton
 from pages.common import Common
 from tests.build_dynamic_arg import build_dynamic_arg_v4
 from pages.Menu.menu import MenuSection
@@ -43,9 +45,63 @@ class TestCommodities:
         test_element = TradeCFDBlockStartTradingNowButton(d, cur_page_url, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_url)
 
-    @allure.step("Start test of button [1. Create & verify your account] in Step trading block")
+    @allure.step("Start test of button [Sell] in Widget 'Trading instrument'")
     @pytest.mark.test_002
-    def test_002_block_step_trading_create_verify_your_account(
+    def test_002_sell_trading_instrument(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_sort):
+        """
+        Check: button [Sell] in Widget 'Trading instrument'
+        Language: All License: All (except FCA) Role: All.
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.05", "Markets > Menu item [Commodities]",
+            ".00_003", "Testing button [Sell]")
+
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
+        cur_page_url = page_menu.sub_menu_commodities_move_focus_click(d, cur_language)
+
+        test_element = TableTradingInstrumentsSellButton(d, cur_page_url, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_url, cur_sort)
+
+    @allure.step("Start test of button [Buy] in Widget 'Trading instrument'")
+    @pytest.mark.test_003
+    def test_003_buy_trading_instrument(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_sort):
+        """
+        Check: button [Buy] in Widget 'Trading instrument'
+        Language: All License: All (except FCA) Role: All.
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.05", "Markets > Menu item [Commodities]",
+            ".00_003", "Testing button [Buy]")
+
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        page_menu.move_focus_to_markets_menu(d, cur_language, cur_country)
+        cur_page_url = page_menu.sub_menu_commodities_move_focus_click(d, cur_language)
+
+        test_element = TableTradingInstrumentsBuyButton(d, cur_page_url, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_url, cur_sort)
+
+    @allure.step("Start test of button [1. Create & verify your account] in Step trading block")
+    @pytest.mark.test_004
+    def test_004_block_step_trading_create_verify_your_account(
             self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
         """
         Check: Button [1. Create & verify account]
