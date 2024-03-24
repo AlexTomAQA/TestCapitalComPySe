2a60177e91b22b7ac5a78b733286c8da044d2230
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-03-25 00:45:59 +0300

    set ro/au+ae

--- conftest.py
+++ conftest.py
@@ -127,9 +127,9 @@ def cur_language(request):
     scope="class",
     params=[
         # "gb",
-        "de",  # Germany  - "CYSEC"
-        # "au",  # Australia - "ASIC"
-        # "ae",  # United Arab Emirates - "SCB"
+        # "de",  # Germany  - "CYSEC"
+        "au",  # Australia - "ASIC"
+        "ae",  # United Arab Emirates - "SCB"
 
         # "gr",  # Greece - "CYSEC"
         # "es",  # Spain - "CYSEC"
