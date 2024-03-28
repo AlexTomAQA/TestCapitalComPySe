7d56863c910a425302161931baeb1bb3c4e2d086
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-03-28 19:48:48 +0300

    set el/de

--- conftest.py
+++ conftest.py
@@ -100,11 +100,11 @@ def cur_role(request):
         # "cn",  # 13 us Education to trade present, financial glossary not present
         # "zh",  # 12 us
         # "fr",  # 11 us
-        "pl",  # 10 us
+        # "pl",  # 10 us
         # "ro",  # 10 us
         # "ar",  # 8 us
         # "nl",  # 8 us
-        # "el",  # 5 us
+        "el",  # 5 us
         # "hu",  # 5 us Magyar
     ],
 )
@@ -127,9 +127,9 @@ def cur_language(request):
     scope="class",
     params=[
         # "gb",
-        # "de",  # Germany  - "CYSEC"
+        "de",  # Germany  - "CYSEC"
         # "au",  # Australia - "ASIC"
-        "ae",  # United Arab Emirates - "SCB"
+        # "ae",  # United Arab Emirates - "SCB"
 
         # "gr",  # Greece - "CYSEC"
         # "es",  # Spain - "CYSEC"
