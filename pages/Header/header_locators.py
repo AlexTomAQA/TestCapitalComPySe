7bd4aa04cea284a7158530b685890810a3a8c62a
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-03-29 21:21:45 +0300

    fix header_locators.py

--- pages/Header/header_locators.py
+++ pages/Header/header_locators.py
@@ -9,4 +9,5 @@ from selenium.webdriver.common.by import By
 class HeaderElementLocators:
 	BUTTON_MY_ACCOUNT = (By.ID, "wg_userarea")
 	MAIN_LOGO_CAPITAL_COM = (By.CSS_SELECTOR, ".cc-header .cc-header__logo")
+	MAIN_LOGO_NEW_CAPITAL_COM = (By.CSS_SELECTOR, '#header [class*="logo_link__wVTFX helpers_hide"]')
 	NEW_MAIN_LOGO_CAPITAL_COM = (By.CSS_SELECTOR, "header .helpers_hideXs__vzgPk.logo_link__wVTFX")
