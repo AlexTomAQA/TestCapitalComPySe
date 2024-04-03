dc915ad081fa2856034c353b85fff0acd46588df
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-04-03 16:30:52 +0300

    fix Trading platform page

--- pages/Capital/Trading_platform/trading_platform_locators.py
+++ pages/Capital/Trading_platform/trading_platform_locators.py
@@ -25,8 +25,8 @@ class TradingInstruments:
     CLOSE_ALL_BUTTON = (By.CSS_SELECTOR, ".visible .ghost")
     BUTTON_CLOSE_ALL = (By.CSS_SELECTOR, ".spotlight > div > button")
     LIST_TRADE_INSTRUMENTS = (By.CSS_SELECTOR, ".tabs-holder segment > .content > .text")
-    # LIST_TRADE_INSTRUMENTS = (By.CSS_SELECTOR, ".tabs-holder .state-item-button .name")
-    SELECTED_TRADE_INSTRUMENT = (By.CSS_SELECTOR, "segment.selected .text")
+    OPEN_TICKET_TRADE_INSTRUMENTS = (By.CSS_SELECTOR, "instrument-chart-view .deal-ticket.opened > div > .title")
+    SELECTED_TRADE_INSTRUMENT = (By.CSS_SELECTOR, "segment.has-border.selected .text")
     # SELECTED_TRADE_INSTRUMENT = (By.CSS_SELECTOR, ".tabs-holder .state-item-button.selected .name")
 
 
