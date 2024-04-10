095e51daa058029c34b3c748cb3f2d34cf8d1b3c
Ivan <ivkedrov@gmail.com>
2024-04-10 20:27:38 +0300

    Added tests 01.01python manage.py runserver_009, 01.01python manage.py runserver_011

--- tests/US_01_Markets/US_01-01_All_markets/US_01-01-01_All_Markets_test.py
+++ tests/US_01_Markets/US_01-01_All_markets/US_01-01-01_All_Markets_test.py
@@ -5,9 +5,11 @@ from pages.Elements.PageInstrumentLongPositionGoToPlatformButton import PageInst
 from pages.Elements.PageInstrumentNotificationButton import PageInstrumentNotificationButton
 from pages.Elements.PageInstrumentShortPositionGoToPlatformButton import PageInstrumentShortPositionGoToPlatformButton
 from pages.Elements.PageInstrumentViewDetailedChartButton import PageInstrumentViewDetailedChartButton
+from pages.Elements.StepTradingBlock import BlockStepTrading
 from pages.Elements.TradeCFDAddToFavouriteButton import TradeCFDAddToFavoriteButton
 from pages.Elements.TradeCFDBuyButton import TradeCFDBuyButton
 from pages.Elements.TradeCFDSellButton import TradeCFDSellButton
+from pages.Elements.TradingCalculatorStartTradingButton import TradingCalculatorStartTradingButton
 from pages.common import Common
 from pages.conditions import Conditions
 from src.src import CapitalComPageSrc
@@ -183,3 +185,49 @@ class TestCommodities:
 
         test_element = PageInstrumentNotificationButton(d, cur_item_link, bid)
         test_element.full_test(d, cur_language, cur_country, cur_role, cur_item_link)
+
+    @allure.step("Start test of button [Start Trading] in the 'Trading calculator' widget on trading instrument page ")
+    @pytest.mark.test_009
+    def test_009_page_trading_instrument_widget_trading_calculator_start_trading_button(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_item_link):
+        """
+        Check: Button [Start Trading] in the 'Trading calculator' widget on trading instrument page
+        Language: All. License: All,except FCA.
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.01", "Markets > Menu item [All markets]",
+            ".01_009", "Testing button [Start Trading] in the 'Trading calculator' "
+                       "widget on trading instrument page")
+
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+
+        page_conditions = Conditions(d, "")
+        page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        test_element = TradingCalculatorStartTradingButton(d, cur_item_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_item_link)
+
+    @allure.step("Start test of button [1. Create & verify your account] in Step trading block")
+    @pytest.mark.test_011
+    def test_011_block_step_trading_create_verify_your_account(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_item_link):
+        """
+        Check: Button [1. Create & verify account]
+        Language: All. License: All, except FCA. Role: All.
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.01", "Markets > Menu item [All markets]",
+            ".01_011", "Testing button [1. Create your account] in Step trading block")
+
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+
+        page_conditions = Conditions(d, "")
+        page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        test_element = BlockStepTrading(d, cur_item_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_item_link)
