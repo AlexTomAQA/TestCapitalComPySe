c112608ed786071340cf3cd6559584de142e316a
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-04-17 12:37:26 +0300

    set it/de

--- conftest.py
+++ conftest.py
@@ -125,11 +125,11 @@ def cur_language(request):
 @pytest.fixture(
     scope="class",
     params=[
-        "gb",  # Great Britain - "FCA"
-        # "de",  # Germany  - "CYSEC"
+        # "gb",  # Great Britain - "FCA"
+        "de",  # Germany  - "CYSEC"
         # "au",  # Australia - "ASIC"
         # "ae",  # United Arab Emirates - "SCB"
-
+        #
         # "gr",  # Greece - "CYSEC"
         # "es",  # Spain - "CYSEC"
         # "fr",  # France - "CYSEC"
