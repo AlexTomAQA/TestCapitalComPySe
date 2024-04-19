d4328239311c708c549dc510adfd663dfe72c6ae
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-04-19 11:21:55 +0300

    set en/gb

--- conftest.py
+++ conftest.py
@@ -91,13 +91,13 @@ def cur_role(request):
 @pytest.fixture(
     scope="class",
     params=[
-        # "",  # "en" - 21 us
+        "",  # "en" - 21 us
         # "ar",  # 8 us
         # "de",  # 15 us
         # "es",  # 20 us
         # "it",  # 15 us
         # "ru",  # 15 us
-        "cn",  # 13 us Education to trade present, financial glossary not present
+        # "cn",  # 13 us Education to trade present, financial glossary not present
         # "zh",  # 12 us
         # "fr",  # 11 us
         # "pl",  # 10 us
@@ -125,10 +125,10 @@ def cur_language(request):
 @pytest.fixture(
     scope="class",
     params=[
-        # "gb",  # Great Britain - "FCA"
+        "gb",  # Great Britain - "FCA"
         # "de",  # Germany  - "CYSEC"
         # "au",  # Australia - "ASIC"
-        "ae",  # United Arab Emirates - "SCB"
+        # "ae",  # United Arab Emirates - "SCB"
         #
         # "gr",  # Greece - "CYSEC"
         # "es",  # Spain - "CYSEC"
