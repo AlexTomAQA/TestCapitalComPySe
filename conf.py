ca6c5791d10de57a33eb8bcbc6951c68b3e95c4e
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-04-22 22:29:41 +0300

    set QTY_LINKS = 1

--- conf.py
+++ conf.py
@@ -4,7 +4,7 @@
 @Author  : Alexander Tomelo
 """
 DEBUG = False
-QTY_LINKS = 2
+QTY_LINKS = 1
 MSG_SKIP_LANGUAGE = 'f"This test-case is not for {cur_language} language"'
 
 HEADLESS = True  # режим браузера без отображения (безголовый)
