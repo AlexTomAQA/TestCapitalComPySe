2ad1cf52d8878ca9f8f7ebdf6b025db0542cfd93
podchasova11 <Pod4asova20@gmail.com>
2024-04-22 12:14:31 +0300

    class recived name and stay in coding process

---
+++ pages/Elements/CFDCalculatorBlockTryFreeDemoButton.py
@@ -0,0 +1,18 @@
+"""
+-*- coding: utf-8 -*-
+@Time    : 2023/04/22 11:30
+@Author  : Mila Podchasova
+"""
+# from datetime import datetime
+# import pytest
+# import allure
+
+# from pages.Elements.AssertClass import AssertClass
+# from pages.Signup_login.signup_login import SignupLogin
+from pages.base_page import BasePage
+# from pages.Markets.markets_locators import BlockCFDCalculator
+# from selenium.common.exceptions import ElementClickInterceptedException
+
+
+class CFDCalculatorBlockSignUpButton(BasePage):
+    pass
