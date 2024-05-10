d7211df9c40123767e131e398a141cc11e4ddee3
unknown <kasila2@rambler.ru>
2024-05-10 15:06:09 +0200

    added new file

--- tests/US_55_ReTestsManual/tassobaccato/US_55-tassobaccato_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/tassobaccato/US_55-tassobaccato_ReTestsManual_test.py
@@ -7,19 +7,37 @@ import pytest
 import allure
 from tests.build_dynamic_arg import build_dynamic_arg_v4
 
+from pages.Elements.MyAccountButton import MyAccountButton
+from pages.common import Common
+from src.src import CapitalComPageSrc
+from tests.ReTestsManual.pages.conditions_new import NewConditions
+
 
 @pytest.mark.us_55
 class TestManualDetectedBugs:
+    page_conditions = None
 
-    @allure.step("Start test of ???")
-    @pytest.mark.test_010
-    def test_010(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
-        # """
-        # Check: Button [1. Create your account] in block [Steps trading]
-        # Language: All. License: All.
-        # """
+    @allure.step("Start retest manual TC_55!00_105 of button [My account] in the Header")
+    @pytest.mark.parametrize('cur_language', [''])
+    @pytest.mark.parametrize('cur_country', ['gb'])
+    @pytest.mark.parametrize('cur_role', ["Auth"])
+    @pytest.mark.test_005
+    def test_005(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+         Check: Button [My account] in the Header
+         Language: En. License: FCA.
+         """
         bid = build_dynamic_arg_v4(
             d, worker_id, cur_language, cur_country, cur_role,
             "55", "ReTest Manual Detected Bugs]",
-            ".005", "??? Description Bug")
-        pytest.skip("Autotest under construction")
+            ".005", "The 'My Account' menu is not displayed when click on the [My Account] button in the Header")
+
+        Common().check_language_in_list_and_skip_if_not_present(cur_language, [''])
+        Common().check_country_in_list_and_skip_if_not_present(cur_country, ['gb'])
+
+        page_conditions = NewConditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL_NEW, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        test_element = MyAccountButton(d, link, bid)
+        test_element.full_test(d, cur_language, cur_country, cur_role, link)
