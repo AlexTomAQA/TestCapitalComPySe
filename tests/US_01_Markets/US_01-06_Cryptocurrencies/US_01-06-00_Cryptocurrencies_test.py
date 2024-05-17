150fbc9cde3e464101df159a928dfd9cbc8da9dc
Amid-Kirdum <mudrik1200@gmail.com>
2024-05-18 00:51:07 +0300

    add AT_002 > test button [Sell]

--- tests/US_01_Markets/US_01-06_Cryptocurrencies/US_01-06-00_Cryptocurrencies_test.py
+++ tests/US_01_Markets/US_01-06_Cryptocurrencies/US_01-06-00_Cryptocurrencies_test.py
@@ -7,6 +7,7 @@ import allure
 import pytest
 
 from pages.Elements.TradeCFDBlockStartTradingNowButton import TradeCFDBlockStartTradingNowButton
+from pages.Elements.TableTradingInstrumentsSellButton import TableTradingInstrumentsSellButton
 from pages.Menu.menu import MenuSection
 from pages.common import Common
 from pages.conditions import Conditions
@@ -45,3 +46,28 @@ class TestCryptocurrencies:
 
         test_element = TradeCFDBlockStartTradingNowButton(d, cur_page_link, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link)
+    @allure.step("Test button [Sell] 'numeric values' in Widget 'Trading instrument'")
+    @pytest.mark.test_002
+    def test_002_sell_widget_trading_instrument(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_sort):
+        """
+        Check: Button [Sell]'numeric values' in Widget 'Trading instrument'
+        Language: All. License: All,except FCA (GB country)
+        """
+        bid = build_dynamic_arg_v4(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "01.06", "Markets > Menu item [Cryptocurrencies]",
+            ".00_002", "Testing button [Sell] 'numeric values' in Widget 'Trading instrument'")
+
+        Common().skip_if_eng_lang_and_fca_license(cur_language, cur_country)
+        Common().check_country_in_list_and_skip_if_present(cur_country, ["gb"])
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        cur_page_link = page_menu.open_cryptocurrencies_market_menu(d, cur_language, cur_country, link)
+
+        test_element = TableTradingInstrumentsSellButton(d, cur_page_link, bid)
+        test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_page_link, cur_sort)
