48a09262648e0edf5dd49b24fde01a4710d9659c
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2024-05-21 07:43:44 +0300

    set it/de

--- conftest.py
+++ conftest.py
@@ -91,11 +91,11 @@ def cur_role(request):
 @pytest.fixture(
     scope="class",
     params=[
-        "",  # "en" - 21 us
+        # "",  # "en" - 21 us
         # "ar",  # 8 us
         # "de",  # 15 us
         # "es",  # 20 us
-        # "it",  # 15 us
+        "it",  # 15 us
         # "ru",  # 15 us
         # "cn",  # 13 us Education to trade present, financial glossary not present
         # "zh",  # 12 us
