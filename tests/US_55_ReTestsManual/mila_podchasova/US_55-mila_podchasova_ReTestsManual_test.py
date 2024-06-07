e2a86e0cf172b26e54bf5cddc80865278429b9ec
podchasova11 <Pod4asova20@gmail.com>
2024-06-07 14:48:12 +0300

    added new test_034

--- tests/US_55_ReTestsManual/mila_podchasova/US_55-mila_podchasova_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/mila_podchasova/US_55-mila_podchasova_ReTestsManual_test.py
@@ -5,36 +5,39 @@
 """
 import pytest
 import allure
+
+from datetime import datetime
+
+from pages.Menu.menu_new import MainMenu
 from pages.build_dynamic_arg import build_dynamic_arg_for_us_55
 
-# from pages.common import Common
-# from src.src import CapitalComPageSrc
-# from pages.conditions_new import NewConditions
+from pages.common import Common
+from src.src import CapitalComPageSrc
+from pages.conditions_new import NewConditions
 
 
 @pytest.mark.us_55
 class TestManualDetectedBugs:
     page_conditions = None
 
-    @allure.step("Start retest manual TC_55!00_027 Icon In Item 'Trade CFDs on margin' in block 'Why trade' is missing")
+    @allure.step("Start retest manual TC_55!00_029 Home page is not opened when click [Platform overview] button")
     @pytest.mark.parametrize('cur_language', [''])
     @pytest.mark.parametrize('cur_country', ['gb'])
     @pytest.mark.parametrize('cur_role', ["NoReg", "Auth", "NoAuth"])
-    @pytest.mark.test_027
-    def test_027(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+    @pytest.mark.test_029
+    def test_029(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
         """
-         Check: Icon In Item 'Trade CFDs on margin'
+         Check: Home page is not opened when click [Platform overview] button
          Language: En. License: FCA.
          """
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
-            "027", "Icon In Item 'Trade CFDs on margin' in block 'Why trade' is missing"
+            "029", "Home page is not opened when click [Platform overview] button"
         )
 
         pytest.skip("Autotest under construction")
 
-        #
         # Common().check_language_in_list_and_skip_if_not_present(cur_language, [''])
         # Common().check_country_in_list_and_skip_if_not_present(cur_country, ['gb'])
         #
@@ -42,7 +45,43 @@ class TestManualDetectedBugs:
         # link = page_conditions.preconditions(
         #     d, CapitalComPageSrc.URL_NEW, "", cur_language, cur_country, cur_role, cur_login, cur_password)
         #
-        # test_element = IconInItemTradeCFDs(d, link, bid)
+        # test_element = PlatformOverviewButton(d, link, bid)
         # test_element.full_test(d, cur_language, cur_country, cur_role, link)
+
+    @allure.step("Start retest manual TC_55!00_034 "
+                 "The footer is missing on click menu item [Professional] of the menu section [Ways to trade]")
+    @pytest.mark.parametrize('cur_language', [''])
+    @pytest.mark.parametrize('cur_country', ['gb'])
+    @pytest.mark.parametrize('cur_role', ["NoReg", "Auth", "NoAuth"])
+    # @allure.severity(allure.severity_level.TRIVIAL)
+    @pytest.mark.test_034
+    def test_34(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+                    Page "The footer is missing on click menu item [Professional] of the menu section [Ways to trade]
+                    1. Hover over the [Ways to trade] menu section
+                    2. Click the [Professional]menu item
+                    """
+        bid = build_dynamic_arg_for_us_55(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "55", "ReTests of Manual Detected Bugs",
+            "034",
+            "The footer is missing on click menu item [Professional] of the menu section [Ways to trade]"
+        )
+        Common().check_language_in_list_and_skip_if_not_present(cur_language, [''])
+        Common().check_country_in_list_and_skip_if_not_present(cur_country, ['gb'])
+
+        page_conditions = NewConditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL_NEW, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        menu = MainMenu(d, link)
+        menu.open_waytotrade_professional_sub_menu(d, cur_language, cur_country, link)
+
+        if not menu.element_is_visible(menu.FOOTER_RISK_WARNING_BLOCK):
+            print(f"{datetime.now()}   => FOOTER_RISK_WARNING_BLOCK is not visible")
+            Common.pytest_fail(
+                "Bug#034. The footer is missing on click menu item [Professional] of the menu section [Ways to trade]")
+        print(f"{datetime.now()}   => The footer is displayed")
+
 
 
