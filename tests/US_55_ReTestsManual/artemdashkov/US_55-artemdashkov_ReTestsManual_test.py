ea9b2c1d7a5aa200c7b6f9a123ba31cff9fddc52
last_name <artemdashkov@gmail.com>
2024-06-08 14:29:29 +0300

    add test_011a and test Class

--- tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
@@ -9,6 +9,7 @@ import pytest
 
 from pages.common import Common
 from pages.Elements.ContentsBlockLearnMoreAboutUsLink import ContentsBlockLearnMoreAboutUsLink
+from pages.Elements.TradePageAddToFavoriteButton import TradePageAddToFavoriteButton
 from pages.Elements.WhyChooseBlockTryNowButtonInContent import WhyChooseBlockTryNowButtonInContent
 from src.src import CapitalComPageSrc
 from pages.build_dynamic_arg import build_dynamic_arg_for_us_55
@@ -86,3 +87,45 @@ class TestManualDetected:
 
         test_element = ContentsBlockLearnMoreAboutUsLink(d, cur_item_link, bid)
         test_element.full_test_with_tpi(d, cur_language, cur_country, cur_role, cur_item_link)
+
+    @allure.step("Start test of button [Add to favourite] on the 'Trading Instrument Page'")
+    @pytest.mark.parametrize('cur_language', ["", "ar", "de", "el", "es", "fr", "it", "hu", "nl", "pl", "ru", "cn", "ro", "zh"])
+    @pytest.mark.parametrize('cur_country', ['de', 'au', 'ae'])
+    @pytest.mark.parametrize('cur_role', ["Auth"])
+    @pytest.mark.parametrize('cur_market', ["Shares", "Forex", "Indices", "Commodities", "Cryptocurrencies"])
+    @pytest.mark.test_011a
+    def test_011a_add_to_favourite_button_on_trading_instrument_page(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_market):
+        """
+        Check: Button [Add to favourite] on the 'Trading Instrument Page'
+        Language: All.
+        License: Not FCA
+        """
+
+        bid = build_dynamic_arg_for_us_55(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "55", "ReTests of Manual Detected Bugs",
+            "011a",
+            "Testing button [Add to favourite] on the 'Trading Instrument Page'",
+            False, False
+        )
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        menu = MenuSection(d, link)
+        match cur_market:
+            case "Shares":
+                cur_item_link = menu.open_shares_market_menu(d, cur_language, cur_country, link)
+            case "Forex":
+                cur_item_link = menu.open_forex_markets_menu(d, cur_language, cur_country, link)
+            case "Indices":
+                cur_item_link = menu.open_indices_markets_menu(d, cur_language, cur_country, link)
+            case "Commodities":
+                cur_item_link = menu.open_commodities_markets_menu(d, cur_language, cur_country, link)
+            case "Cryptocurrencies":
+                cur_item_link = menu.open_cryptocurrencies_market_menu(d, cur_language, cur_country, link)
+
+        test_element = TradePageAddToFavoriteButton(d, cur_item_link, bid)
+        test_element.full_test(d, cur_language, cur_country, cur_role, cur_item_link, cur_market)
