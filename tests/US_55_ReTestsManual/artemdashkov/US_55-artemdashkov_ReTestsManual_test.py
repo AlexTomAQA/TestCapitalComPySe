eecb7b4ca0f499875da976003601b4bddff3b5ba
last_name <artemdashkov@gmail.com>
2024-06-09 17:42:55 +0300

    add test_011b, US_55

--- tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
@@ -11,6 +11,7 @@ from pages.common import Common
 from pages.Elements.ContentsBlockLearnMoreAboutUsLink import ContentsBlockLearnMoreAboutUsLink
 from pages.Elements.TradePageAddToFavoriteButton import TradePageAddToFavoriteButton
 from pages.Elements.WhyChooseBlockTryNowButtonInContent import WhyChooseBlockTryNowButtonInContent
+from pages.Elements.PageInstrumentLongPositionGoToPlatformButton import PageInstrumentLongPositionGoToPlatformButton
 from src.src import CapitalComPageSrc
 from pages.build_dynamic_arg import build_dynamic_arg_for_us_55
 from pages.conditions import Conditions
@@ -129,3 +130,49 @@ class TestManualDetected:
 
         test_element = TradePageAddToFavoriteButton(d, cur_item_link, bid)
         test_element.full_test(d, cur_language, cur_country, cur_role, cur_item_link, cur_market)
+
+    @allure.step("Start test of button [Go to platform] on tooltip 'Long position overnight fee'")
+    @pytest.mark.parametrize('cur_language', ["", "ar", "de", "el", "es", "fr", "it", "hu", "nl", "pl", "ru", "cn", "ro", "zh"])
+    @pytest.mark.parametrize('cur_country', ['de', 'au', 'ae'])
+    @pytest.mark.parametrize('cur_role', ["Auth"])
+    @pytest.mark.parametrize('cur_market', ["Shares", "Forex", "Indices", "Commodities", "Cryptocurrencies"])
+    @pytest.mark.test_011b
+    def test_011b_add_to_favourite_button_on_tooltip_long_position_overnight_fee(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_market):
+        """
+        Check: Button [Go to platform] on tooltip 'Long position overnight fee'
+        Language: All.
+        License: Not FCA
+        """
+
+        bid = build_dynamic_arg_for_us_55(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "55", "ReTests of Manual Detected Bugs",
+            "011b",
+            "Testing button [Go to platform] on tooltip 'Long position overnight fee'",
+            False, False
+        )
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        menu = MenuSection(d, link)
+        match cur_market:
+            case "Shares":
+                cur_item_link = menu.open_shares_market_menu(d, cur_language, cur_country, link)
+            case "Forex":
+                cur_item_link = menu.open_forex_markets_menu(d, cur_language, cur_country, link)
+            case "Indices":
+                cur_item_link = menu.open_indices_markets_menu(d, cur_language, cur_country, link)
+            case "Commodities":
+                cur_item_link = menu.open_commodities_markets_menu(d, cur_language, cur_country, link)
+            case "Cryptocurrencies":
+                cur_item_link = menu.open_cryptocurrencies_market_menu(d, cur_language, cur_country, link)
+
+        test_element = TradePageAddToFavoriteButton(d, cur_item_link, bid)
+        test_element.arrange_1(d, cur_item_link, cur_market)
+
+        cur_item_link = d.current_url
+        test_element = PageInstrumentLongPositionGoToPlatformButton(d, cur_item_link, bid)
+        test_element.full_test_with_tpi_v2(d, cur_language, cur_country, cur_role, cur_item_link)
