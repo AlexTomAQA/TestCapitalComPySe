ff6c3ca134072395719124c32913bd87a4d93b00
Kasilà <kasila2@rambler.ru>
2024-06-18 22:00:28 +0200

    added file

--- tests/US_55_ReTestsManual/tassobaccato/US_55-tassobaccato_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/tassobaccato/US_55-tassobaccato_ReTestsManual_test.py
@@ -5,10 +5,13 @@
 """
 import pytest
 import allure
+
+from pages.Menu.menu import MenuSection
 from pages.build_dynamic_arg import build_dynamic_arg_for_us_55
 
 from pages.Elements.MyAccountButton import MyAccountButton
 from pages.common import Common
+from pages.conditions import Conditions
 from src.src import CapitalComPageSrc
 from pages.conditions_new import NewConditions
 
@@ -43,3 +46,32 @@ class TestManualDetectedBugs:
 
         test_element = MyAccountButton(d, link, bid)
         test_element.full_test(d, cur_language, cur_country, cur_role, link)
+
+    @allure.step("Start retest manual TC_55!00_039 of filters application in the 'Live shares prices' widget")
+    @pytest.mark.parametrize('cur_country', ['es', 'ae', 'au'])
+    @pytest.mark.parametrize('cur_role', ["NoReg", "Auth", "NoAuth"])
+    @pytest.mark.test_039
+    def test_039(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+        Check: Filters application in the 'Live shares prices' widget
+        Language: All
+        License: All (except FCA)
+        """
+
+        bid = build_dynamic_arg_for_us_55(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "55", "ReTests of Manual Detected Bugs",
+            "039", "Applied filters 'Region/Sectors' are not displayed after selecting an item from the "
+                   "'Most traded' dropdown in the 'Live shares prices'  widget on the 'Shares' page"
+            )
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        menu = MenuSection(d, link)
+        cur_item_link = menu.open_shares_market_menu(d, cur_language, cur_country, link)
+
+
+#        assert len(selected_filters_list) == len(filters_list_after_refresh), \
+#            'Applied filters "Region/Sectors" are not displayed in the "Live shares prices" widget'
