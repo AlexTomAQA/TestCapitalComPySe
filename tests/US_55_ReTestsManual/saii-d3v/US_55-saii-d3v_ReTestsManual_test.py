666c246b7b66d7314a39356342e47816739a41c2
saii-d3v <saii.d3v@gmail.com>
2024-06-22 20:36:48 +0500

    intermediate commit

--- tests/US_55_ReTestsManual/saii-d3v/US_55-saii-d3v_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/saii-d3v/US_55-saii-d3v_ReTestsManual_test.py
@@ -7,10 +7,13 @@
 import pytest
 import allure
 
+from datetime import datetime
+
 from pages.common import Common                                      # check FCA, check Auth
-from pages.base_page import BasePage                                 # accept cookies
-from pages.conditions import Conditions
+from pages.conditions import Conditions                              # accept cookies
+from pages.base_page import BasePage
 from pages.build_dynamic_arg import build_dynamic_arg_for_us_55
+from pages.BugsManual.bug_043 import SearchField
 from pages.Signup_login.signup_login import SignupLogin
 from pages.Elements.HeaderLoginButton import HeaderButtonLogin
 from src.src import CapitalComPageSrc
@@ -21,10 +24,10 @@ class TestManualDetectedBugs:
     page_conditions = None
 
     @allure.step("Start retest manual TC_55!0043 The page is refreshed instead of opening the Login form after clicking the [Log In] button on the Search page")
-    @pytest.mark.parametrize('cur_country', ['au', 'ae', 'de'])
+    @pytest.mark.parametrize('cur_country', ['au', 'ax', 'de'])
     @pytest.mark.parametrize('cur_role', ["NoReg", "NoAuth"])
     @pytest.mark.test_043
-    def test_043(self, worker_id, d, cur_language_3_rnd_from_14, cur_country, cur_role, cur_login, cur_password):
+    def test_043(self, worker_id, d, cur_language_3_rnd_from_14, cur_country, cur_role, cur_login, cur_password, random_search_string):
         """
          Check: The page is refreshed instead of opening the Login form after clicking the [Log In] button on the Search page
          Language: All.
@@ -38,10 +41,13 @@ class TestManualDetectedBugs:
             "The page is refreshed instead of opening the Login form after clicking the [Log In] button on the Search page"
         )
 
-        pytest.skip("Autotest under construction")
+        # pytest.skip("Autotest under construction")
 
-        # page_conditions = Conditions(d, "")  # проверить без явного указания ссылки
-        # link = page_conditions.preconditions(
-        #     d, CapitalComPageSrc.URL, "", cur_language_3_rnd_from_14, cur_country, cur_role, cur_login, cur_password)
-        #
+        page_conditions = Conditions(d, "")  # проверить без явного указания ссылки
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language_3_rnd_from_14, cur_country, cur_role, cur_login, cur_password)
 
+        Common().check_country_in_list_and_skip_if_present(cur_country, ['gb', 'ae'])
+
+        search_field = SearchField(d, link, bid)
+        search_field.open_search_page(random_search_string)
