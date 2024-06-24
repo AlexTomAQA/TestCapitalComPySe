aa210aa79f7088b0c3cfa71fc9a5b49001a41c90
saii-d3v <saii.d3v@gmail.com>
2024-06-24 22:31:03 +0500

    AT is ready to review

--- pages/BugsManual/bug_043.py
+++ pages/BugsManual/bug_043.py
@@ -6,7 +6,6 @@
 
 from datetime import datetime
 
-from pages.common import Common
 from pages.base_page import BasePage
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,11 +20,12 @@ class SearchField(BasePage):
 
         super().__init__(browser, link, bid)
 
-    def open_search_page(self):
+    def open_search_page(self, search_string):
         print(f'\n{datetime.now()}   1. Arrange')
         print(f'\n{datetime.now()}   Opening the Search page...')
         self.search_btn = Wait(self.driver, 5).until(EC.element_to_be_clickable(BUTTON_LOCATOR))
         self.search_btn.click()
-        self.search_btn.send_keys("123")
+        print(f'\n{datetime.now()}   Current search string: {search_string}')
+        self.search_btn.send_keys(search_string)
         self.search_btn.submit()
-        print(f'\n{datetime.now()}   Done')
+        print(f'\n{datetime.now()}   Search page is opened.')
