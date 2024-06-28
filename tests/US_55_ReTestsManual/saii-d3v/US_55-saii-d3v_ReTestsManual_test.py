888a198bb23d2657f1396bc623cdb5c1f26d773b
saii-d3v <saii.d3v@gmail.com>
2024-06-28 10:05:10 +0500

    refactoring

--- tests/US_55_ReTestsManual/saii-d3v/US_55-saii-d3v_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/saii-d3v/US_55-saii-d3v_ReTestsManual_test.py
@@ -84,9 +84,6 @@ class TestManualDetectedBugs:
          License: CYSEC.
          Author: Sergey Aiidzhanov
          """
-
-        Common().check_language_in_list_and_skip_if_present(cur_language_3_rnd_from_14, ['el'])
-
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language_3_rnd_from_14, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
@@ -94,6 +91,9 @@ class TestManualDetectedBugs:
             "Login form is opened instead of Sign up form after clicking the button [Apply] "
             "in the Block 'Leverage Limits Professional Clients' on page 'Professional Account'"
         )
+
+        # Bug is not reproduced in 'el' language
+        Common().check_language_in_list_and_skip_if_present(cur_language_3_rnd_from_14, ['el'])
 
         page_conditions = Conditions(d)
 
@@ -110,6 +110,3 @@ class TestManualDetectedBugs:
         # Assert
         signup_form = SignupLogin(d, link, bid)
         assert signup_form.should_be_signup_form(cur_language_3_rnd_from_14)
-
-
-        # pytest.skip("AT is under construction")
