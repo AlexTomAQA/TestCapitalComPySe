a8936a240e0f949bd8a4e969dcb95aaa534c10d2
last_name <artemdashkov@gmail.com>
2024-07-14 19:52:18 +0300

    complete test_095 and test class

--- pages/BugsManual/bug_095.py
+++ pages/BugsManual/bug_095.py
@@ -5,8 +5,11 @@
 """
 import allure
 from datetime import datetime
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
 
+from pages.Signup_login.signup_login import SignupLogin
 from pages.base_page import BasePage
 from pages.common import Common
 
@@ -20,7 +23,7 @@ class BUG_095(BasePage):
         super().__init__(browser, link, bid)
 
     @allure.step(f"{datetime.now()}   1. Start Arrange.")
-    def arrange(self):
+    def arrange(self, d):
         print(f"{datetime.now()}   1. Start Arrange.")
 
         #Check presenting SEARCH FIELD
@@ -53,18 +56,51 @@ class BUG_095(BasePage):
             Common().pytest_fail(msg)
         print(f"{datetime.now()}   => {BLOCK_NAME} is clickable on this page!\n")
 
-    @allure.step(f"{datetime.now()}   1. Start Arrange.")
-    def act(self, ):
-        print(f"{datetime.now()}   1. Start Act.")
+    @allure.step(f"{datetime.now()}   2. Start Act.")
+    def act(self, d, search_query):
+        print(f"{datetime.now()}   2. Start Act.")
 
         # Start click on SEARCH FIELD
         print(f"{datetime.now()}   Start click on {BLOCK_NAME} =>")
-        self.driver.click(*SEARCH_FIELD_LOCATOR)
+        try:
+            self.driver.find_element(*SEARCH_FIELD_LOCATOR).click()
+            print(f"{datetime.now()}   => {BLOCK_NAME} clicked!\n")
+
+        except ElementClickInterceptedException:
+            print(f"{datetime.now()}   => {BLOCK_NAME} NOT CLICKED")
+
+            print(f"{datetime.now()}   'Sign up' form or page is auto opened")
+
+            page_ = SignupLogin(self.driver)
+            if page_.close_signup_form():
+                pass
+            elif page_.close_login_form():
+                pass
+            elif page_.close_signup_page():
+                pass
+            else:
+                page_.close_login_page()
+
+        self.driver.find_element(*SEARCH_FIELD_LOCATOR).click()
         print(f"{datetime.now()}   => {BLOCK_NAME} clicked!\n")
 
         # Start write search query
         print(f"{datetime.now()}   Start write search query in {BLOCK_NAME} =>")
-        self.driver.send_keys(*SEARCH_FIELD_LOCATOR)
-        print(f"{datetime.now()}   => {BLOCK_NAME} clicked!\n")
+        self.driver.find_element(*SEARCH_FIELD_LOCATOR).send_keys(search_query)
+        Common.save_current_screenshot(d, f"Put search_query: '{search_query}' in Search field!")
+        print(f"{datetime.now()}   => Put search_query: '{search_query}' in Search field!\n")
 
+        # Press Enter
+        self.driver.find_element(*SEARCH_FIELD_LOCATOR).send_keys(Keys.ENTER)
+        print(f"{datetime.now()}   => Key 'Enter' pressed!\n")
 
+    @allure.step(f"{datetime.now()}   3. Start Assert.")
+    def assert_(self, d, cur_item_link):
+        print(f"{datetime.now()}   3. Start Assert.")
+        if self.driver.current_url == cur_item_link:
+            msg = f"Item link didn't change. Search options doesn't work properly!"
+            print(f"{datetime.now()}   => {msg}\n")
+            Common().pytest_fail(msg)
+        Common.save_current_screenshot(d, "Item link change. Need to analyze result.")
+        print(f"{datetime.now()}   => Item link change. But need to analyze result.\n")
+        return True
