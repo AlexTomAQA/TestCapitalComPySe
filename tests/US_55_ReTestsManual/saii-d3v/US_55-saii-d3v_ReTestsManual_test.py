f46c9db723b6a53c9933c6cb390bb935cb68daf6
saii-d3v <saii.d3v@gmail.com>
2024-07-16 11:06:13 +0500

    update ID

--- tests/US_55_ReTestsManual/saii-d3v/US_55-saii-d3v_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/saii-d3v/US_55-saii-d3v_ReTestsManual_test.py
@@ -14,9 +14,9 @@ from pages.common import Common
 from pages.conditions import Conditions
 from pages.build_dynamic_arg import build_dynamic_arg_for_us_55
 
-from pages.BugsManual.bug_060 import ProfessionalAccountPage
-from pages.BugsManual.bug_065 import TradingGuidesPageDeTest
-from pages.BugsManual.bug_091 import NewsAndAnalysisMenuSection
+from pages.BugsManual.bug_076 import ProfessionalAccountPage
+from pages.BugsManual.bug_085 import TradingGuidesPageDeTest
+from pages.BugsManual.bug_158 import NewsAndAnalysisMenuSection
 from pages.Elements.HeaderSearchField import SearchField
 from pages.Signup_login.signup_login import SignupLogin
 from pages.Elements.HeaderLoginButton import HeaderButtonLogin
@@ -33,8 +33,8 @@ class TestManualDetectedBugs:
                  "form after clicking the [Log In] button on the Search page")
     @pytest.mark.parametrize('cur_country', ['de', 'ua', 'au'])
     @pytest.mark.parametrize('cur_role', ['NoAuth', 'NoReg'])
-    @pytest.mark.test_043
-    def test_043(self, worker_id, d, cur_language_qty_rnd_from_14, cur_country, cur_role,
+    @pytest.mark.bug_053
+    def test_053(self, worker_id, d, cur_language_qty_rnd_from_14, cur_country, cur_role,
                  cur_login, cur_password, random_search_string):
         """
          Check: The page is refreshed instead of opening the Login form after clicking the [Log In] button
@@ -47,7 +47,7 @@ class TestManualDetectedBugs:
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language_qty_rnd_from_14, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
-            "043",
+            "053",
             "The page is refreshed instead of opening the Login form after clicking the [Log In] button "
             "on the Search page",
             False,
@@ -77,21 +77,21 @@ class TestManualDetectedBugs:
         print(f'\n{datetime.now()}   3. Assert')
         login_form = SignupLogin(d, link, bid)
         if not login_form.should_be_login_form():
-            Common().pytest_fail(f"Bug # 55!043   The Login form is NOT displayed")
-        Common().save_current_screenshot(d, "AT_55!043 Pass")
+            Common().pytest_fail(f"Bug # 55!053   The Login form is NOT displayed")
+        Common().save_current_screenshot(d, "AT_55!053 Pass")
 
         # Postconditions
         print(f'\n{datetime.now()}   Applying postconditions...')
         login_form.close_login_form()
         Common().browser_back_to_link(d, CapitalComPageSrc.URL)
 
-    @allure.step("Start retest manual TC_55!060 Login form is opened instead of Sign up form "
+    @allure.step("Start retest manual TC_55!076 Login form is opened instead of Sign up form "
                  "after clicking the button [Apply] in the Block 'Leverage Limits Professional Clients' "
                  "on page 'Professional Account'")
     @pytest.mark.parametrize('cur_country', ['de'])
     @pytest.mark.parametrize('cur_role', ['NoReg'])
-    @pytest.mark.test_060
-    def test_060(self, worker_id, d, cur_language_qty_rnd_from_14, cur_country, cur_role, cur_login, cur_password):
+    @pytest.mark.bug_076
+    def test_076(self, worker_id, d, cur_language_qty_rnd_from_14, cur_country, cur_role, cur_login, cur_password):
         """
          Check: Login form is opened instead of Sign up form after clicking the button [Apply]
                 in the Block 'Leverage Limits Professional Clients' on page 'Professional Account'
@@ -102,7 +102,7 @@ class TestManualDetectedBugs:
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language_qty_rnd_from_14, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
-            "060",
+            "076",
             "Login form is opened instead of Sign up form after clicking the button [Apply] "
             "in the Block 'Leverage Limits Professional Clients' on page 'Professional Account'"
         )
@@ -128,22 +128,22 @@ class TestManualDetectedBugs:
         print(f'\n{datetime.now()}   3. Assert')
         signup_form = SignupLogin(d, link, bid)
         if not signup_form.should_be_signup_form(cur_language_qty_rnd_from_14):
-            Common().pytest_fail("Bug # 55!060   The Signup form is NOT displayed")
-        Common().save_current_screenshot(d, "AT_55!060 Pass")
+            Common().pytest_fail("Bug # 55!076   The Signup form is NOT displayed")
+        Common().save_current_screenshot(d, "AT_55!076 Pass")
 
         # Postconditions
         print(f'\n{datetime.now()}   Applying postconditions...')
         signup_form.close_signup_form()
         Common().browser_back_to_link(d, CapitalComPageSrc.URL)
 
-    @allure.step("Start retest manual TC_55!065 The Trading Guides page is not opened "
+    @allure.step("Start retest manual TC_55!085 The Trading Guides page is not opened "
                  "after clicking the link [Handelsleitfäden] (trading guides) "
                  "on the page [Demo-Konto] (Demo Account) in DE lang")
     @pytest.mark.parametrize('cur_language', ['de'])
     @pytest.mark.parametrize('cur_country', ['de', 'ua', 'au'])
     @pytest.mark.parametrize('cur_role', ['Auth', 'NoAuth', 'NoReg'])
-    @pytest.mark.bug_065
-    def test_065(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+    @pytest.mark.bug_085
+    def test_085(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
         """
          Check: The Trading Guides page is not opened after clicking the link [Handelsleitfäden] (trading guides)
          on the page [Demo-Konto] (Demo Account) in DE lang
@@ -154,7 +154,7 @@ class TestManualDetectedBugs:
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
-            "065",
+            "085",
             "The Trading Guides page is not opened after clicking the link [Handelsleitfäden] (trading guides) "
             "on the page [Demo-Konto] (Demo Account) in DE lang"
         )
@@ -178,21 +178,21 @@ class TestManualDetectedBugs:
 
         # Assert
         if not trading_guides_page.should_be_trading_guides_page():
-            Common().pytest_fail("Bug # 55!065 The Trading Guides page is NOT opened")
-        Common().save_current_screenshot(d, "AT_55!065 Pass")
+            Common().pytest_fail("Bug # 55!085 The Trading Guides page is NOT opened")
+        Common().save_current_screenshot(d, "AT_55!085 Pass")
 
         # Postconditions
         print(f'\n{datetime.now()}   Applying postconditions...')
         Common().browser_back_to_link(d, CapitalComPageSrc.URL)
 
     @allure.step(
-        'Start retest manual TC_55!090 The modal window "Confirm Form Resubmission" is not opened '
+        'Start retest manual TC_55!157 The modal window "Confirm Form Resubmission" is not opened '
         'after clicking the button [Back] on any article from search page.')
     @pytest.mark.parametrize('cur_language', [''])
     @pytest.mark.parametrize('cur_country', ['de', 'ua', 'au'])
     @pytest.mark.parametrize('cur_role', ['Auth', 'NoAuth', 'NoReg'])
-    @pytest.mark.test_090
-    def test_090(self, worker_id, d, cur_language, cur_country, cur_role,
+    @pytest.mark.bug_157
+    def test_157(self, worker_id, d, cur_language, cur_country, cur_role,
                  cur_login, cur_password, random_search_string):
         """
          Check: The modal window "Confirm Form Resubmission" is not opened after clicking the button [Back]
@@ -205,7 +205,7 @@ class TestManualDetectedBugs:
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
-            "090",
+            "157",
             'The modal window "Confirm Form Resubmission" is not opened after clicking the button [Back] '
             'on any article from search page.',
             False,
@@ -235,8 +235,8 @@ class TestManualDetectedBugs:
 
         alert = Alert(d, link, bid)
         if not alert.should_be_alert():
-            Common().pytest_fail('Bug # 55!090 The modal window "Confirm Form Resubmission" is NOT opened')
-        Common().save_current_screenshot(d, "AT_55!090 Pass")
+            Common().pytest_fail('Bug # 55!157 The modal window "Confirm Form Resubmission" is NOT opened')
+        Common().save_current_screenshot(d, "AT_55!157 Pass")
 
         # Postconditions
         print(f'\n{datetime.now()}   Applying postconditions...')
@@ -244,13 +244,13 @@ class TestManualDetectedBugs:
         Common().browser_back_to_link(d, CapitalComPageSrc.URL)
 
     @allure.step(
-        "Start retest manual TC_55!091 | Page '教育' (Education) is opened after click "
+        "Start retest manual TC_55!158 | Page '教育' (Education) is opened after click "
         "on menu section [新聞和分析] (News and analysis) in CN language")
     @pytest.mark.parametrize('cur_language', ['cn'])
     @pytest.mark.parametrize('cur_country', ['de', 'ua', 'au'])
     @pytest.mark.parametrize('cur_role', ['Auth', 'NoAuth', 'NoReg'])
-    @pytest.mark.test_091
-    def test_091(self, worker_id, d, cur_language, cur_country, cur_role,
+    @pytest.mark.bug_158
+    def test_158(self, worker_id, d, cur_language, cur_country, cur_role,
                  cur_login, cur_password):
         """
          Check: Page '教育' (Education) is opened after click on menu section [新聞和分析] (News and analysis) in CN language.
@@ -262,8 +262,8 @@ class TestManualDetectedBugs:
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
-            "091",
-            "Start retest manual TC_55!091 | Page '教育' (Education) is opened after click "
+            "158",
+            "Page '教育' (Education) is opened after click "
             "on menu section [新聞和分析] (News and analysis) in CN language",
             False,
             False
@@ -284,18 +284,18 @@ class TestManualDetectedBugs:
 
         # Assert
         if not news_and_analysis_menu_section.should_be_news_and_analysis_page():
-            Common().pytest_fail("Bug # 55!091 The News and Analysis page is NOT opened")
-        Common().save_current_screenshot(d, "AT_55!091 Pass")
+            Common().pytest_fail("Bug # 55!158 The News and Analysis page is NOT opened")
+        Common().save_current_screenshot(d, "AT_55!158 Pass")
 
         # Postconditions
         print(f'\n{datetime.now()}   Applying postconditions...')
         Common().browser_back_to_link(d, CapitalComPageSrc.URL)
 
-    @allure.step("Start retest manual TC_55!093 The Search field in the header is not opened after performed search")
+    @allure.step("Start retest manual TC_55!160 The Search field in the header is not opened after performed search")
     @pytest.mark.parametrize('cur_country', ['de', 'ua', 'au'])
     @pytest.mark.parametrize('cur_role', ['Auth', 'NoAuth', 'NoReg'])
-    @pytest.mark.test_093
-    def test_093(self, worker_id, d, cur_language_qty_rnd_from_14, cur_country, cur_role,
+    @pytest.mark.test_160
+    def test_160(self, worker_id, d, cur_language_qty_rnd_from_14, cur_country, cur_role,
                  cur_login, cur_password, random_search_string):
         """
          Check: The Search field in the header is not opened after performed search
@@ -307,7 +307,7 @@ class TestManualDetectedBugs:
         bid = build_dynamic_arg_for_us_55(
             d, worker_id, cur_language_qty_rnd_from_14, cur_country, cur_role,
             "55", "ReTests of Manual Detected Bugs",
-            "093",
+            "160",
             "The Search field in the header is not opened after performed search",
             False,
             False
@@ -334,8 +334,8 @@ class TestManualDetectedBugs:
         time.sleep(1)
 
         if not search_field.should_be_active_search_field():
-            Common().pytest_fail("Bug # 55!093 The Search field in the header is NOT active")
-        Common().save_current_screenshot(d, "AT_55!093 Pass")
+            Common().pytest_fail("Bug # 55!160 The Search field in the header is NOT active")
+        Common().save_current_screenshot(d, "AT_55!160 Pass")
 
         # Postconditions
         print(f'\n{datetime.now()}   Applying postconditions...')
