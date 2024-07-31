6d289efc5f7d66348019b8daff870d1f6fdcb9b5
Alexander Tomelo <qa.tomelo.an@gmail.com>
2024-07-31 20:17:40 +0300

    fix from_trading_menu_open_web_platform

--- pages/Menu/New/from_trading_menu_open_web_platform.py
+++ pages/Menu/New/from_trading_menu_open_web_platform.py
@@ -11,7 +11,7 @@ import allure
 from pages.base_page import BasePage
 from pages.common import Common
 from pages.Menu.New.menu_new_base import MenuBase
-from pages.Menu.New.menu_new_locators import MenuTrading
+from pages.Menu.New.menu_new_locators import TradingMenuNew
 
 
 class MenuNew(MenuBase):
@@ -20,9 +20,9 @@ class MenuNew(MenuBase):
     def from_trading_menu_open_web_platform(self, d, cur_language, cur_country, link):
 
         menu_name = "Trading"
-        menu_locator = MenuTrading.MENU_SCA_TRADING
+        menu_locator = TradingMenuNew.MENU_SCA_TRADING
         submenu_name = "Web platform"
-        submenu_locator = MenuTrading.SUB_MENU_SCA_WEB_PLATFORM
+        submenu_locator = TradingMenuNew.SUB_MENU_SCA_WEB_PLATFORM
         answer = MenuBase(d, link).move_focus_menu_pause_move_focus_to_submenu_and_click(
             d, link, cur_language, cur_country, menu_name, menu_locator, submenu_name, submenu_locator)
 
