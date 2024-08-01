7621e9bf84f9fc3e499cde993ec9734ecf1b3b8e
saii-d3v <saii.d3v@gmail.com>
2024-08-01 20:57:19 +0500

    add method from_trading_menu_open_trading

---
+++ pages/Menu/New/from_trading_menu_open_trading.py
@@ -0,0 +1,36 @@
+"""
+-*- coding: utf-8 -*-
+@Time    : 2024/08/01 18:00 GMT+5
+@Author  : Sergey Aiidzhanov
+"""
+import allure
+
+from pages.Menu.New.menu_new_base import MenuBase
+
+trading_menu_loc_fca = ("css selector", ".menuGroup_linkfirstLevel__d5JGC > [data-type='nav_id686']")
+trading_menu_loc_sca = ("css selector", ".menuGroup_linkfirstLevel__d5JGC > [data-type='nav_id798']")
+trading_submenu_loc_fca = ("css selector", ".helpers_showLg__UPEqH > [data-type='nav_id686']")
+trading_submenu_loc_sca = ("css selector", ".helpers_showLg__UPEqH > [data-type='nav_id798']")
+
+
+class MenuNew(MenuBase):
+
+    @allure.step('Select "Trading" menu, "Trading" submenu')
+    def from_trading_menu_open_trading(self, d, cur_language, cur_country, link):
+
+        menu_name = "Trading"
+        menu_locator = ""
+        submenu_name = "Trading"
+        submenu_locator = ""
+
+        if cur_country == 'gb':
+            menu_locator = trading_menu_loc_fca
+            submenu_locator = trading_submenu_loc_fca
+        if cur_country == 'ae':
+            menu_locator = trading_menu_loc_sca
+            submenu_locator = trading_submenu_loc_sca
+
+        answer = MenuBase(d, link).move_focus_menu_pause_move_focus_to_submenu_and_click(
+            d, link, cur_language, cur_country, menu_name, menu_locator, submenu_name, submenu_locator)
+
+        return answer
