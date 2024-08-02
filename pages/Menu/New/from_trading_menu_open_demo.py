4d969ca3a9abc3e393caa8c7f0608a6c04e1a34f
podchasova11 <Pod4asova20@gmail.com>
2024-08-02 22:14:25 +0300

    refactoring

--- pages/Menu/New/from_trading_menu_open_demo.py
+++ pages/Menu/New/from_trading_menu_open_demo.py
@@ -8,11 +8,7 @@
 import allure
 
 from pages.Menu.New.menu_new_base import MenuBase
-
-MENU_FCA_TRADING = ("css selector", ".menuGroup_linkfirstLevel__d5JGC > [data-type='nav_id686']")
-MENU_SCA_TRADING = ("css selector", ".menuGroup_linkfirstLevel__d5JGC > [data-type='nav_id798']")
-SUB_MENU_FCA_DEMO_EN = ("css selector", "div.grid_grid__2D3md > a[data-type='nav_id751']")
-SUB_MENU_SCA_DEMO_EN = ("css selector", "div.grid_grid__2D3md > a[data-type='nav_id1029']")
+from pages.Menu.New.menu_new_locators import TradingMenuNew
 
 
 class MenuNewDemo(MenuBase):
@@ -25,11 +21,11 @@ class MenuNewDemo(MenuBase):
         submenu_locator = ''
 
         if cur_country == 'gb':
-            menu_locator = MENU_FCA_TRADING
-            submenu_locator = SUB_MENU_FCA_DEMO_EN
+            menu_locator = TradingMenuNew.MENU_FCA_TRADING
+            submenu_locator = TradingMenuNew.SUB_MENU_FCA_DEMO_EN
         if cur_country == 'ae':
-            menu_locator = MENU_SCA_TRADING
-            submenu_locator = SUB_MENU_SCA_DEMO_EN
+            menu_locator = TradingMenuNew.MENU_SCA_TRADING
+            submenu_locator = TradingMenuNew.SUB_MENU_SCA_DEMO_EN
 
         answer = MenuBase(d, link).move_focus_menu_pause_move_focus_to_submenu_and_click(
             d, link, cur_language, cur_country, menu_name, menu_locator, submenu_name, submenu_locator)
