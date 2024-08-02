43fe131339830d3b8dce97794a8067d2f9bee0fc
podchasova11 <Pod4asova20@gmail.com>
2024-08-02 23:19:23 +0300

    rewrite some locators

--- pages/Menu/New/from_markets_menu_open_markets.py
+++ pages/Menu/New/from_markets_menu_open_markets.py
@@ -6,10 +6,8 @@
 import allure
 
 from pages.Menu.New.menu_new_base import MenuBase
+from pages.Menu.New.menu_new_locators import MarketsMenuNew
 
-MARKETS_MENU_FCA_LOCATOR = ("css selector", "span > a[data-type='nav_id824']")
-MARKETS_MENU_SCA_LOCATOR = ("css selector", "span > a[data-type='nav_id824']")
-MARKETS_MENU_SCA_AR_LOCATOR = ("css selector", "span > a[data-type='nav_id824']")
 MARKETS_SUBMENU_FCA_LOCATOR = ("css selector", "div:nth-child(2) > div.menuGroup_dropdown__75ey5 a > div > h2")
 MARKETS_SUBMENU_SCA_LOCATOR = ("css selector", "div:nth-child(2) > div.menuGroup_dropdown__75ey5 a > div > h2")
 MARKETS_SUBMENU_SCA_AR_LOCATOR = ("css selector", 'div:nth-child(2) > div.menuGroup_dropdown__75ey5 a > div > h2')
@@ -26,13 +24,13 @@ class MenuNewMarkets(MenuBase):
         submenu_locator = ""
 
         if cur_country == 'gb':
-            menu_locator = MARKETS_MENU_FCA_LOCATOR
+            menu_locator = MarketsMenuNew.MENU_FCA_MARKETS
             submenu_locator = MARKETS_SUBMENU_FCA_LOCATOR
         elif cur_country == 'ae':
-            menu_locator = MARKETS_MENU_SCA_LOCATOR
+            menu_locator = MarketsMenuNew.MENU_SCA_MARKETS
             submenu_locator = MARKETS_SUBMENU_SCA_LOCATOR
         elif cur_country == 'ae' and cur_language == 'ar':
-            menu_locator = MARKETS_MENU_SCA_AR_LOCATOR
+            menu_locator = MarketsMenuNew.MENU_SCA_MARKETS
             submenu_locator = MARKETS_SUBMENU_SCA_AR_LOCATOR
 
         answer = MenuBase(d, link).move_focus_menu_pause_move_focus_to_submenu_and_click(
