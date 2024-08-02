a822ce77bb6fec9514b32ae0ff096b6d84fafbad
Alexander Tomelo <qa.tomelo.an@gmail.com>
2024-08-02 23:38:51 +0300

    rf menu_new_locators

--- pages/Menu/New/menu_new_locators.py
+++ pages/Menu/New/menu_new_locators.py
@@ -8,20 +8,14 @@ class TradingMenuNew:
     MENU_FCA_TRADING = (
         By.CSS_SELECTOR, '.menuGroup_linkfirstLevel__d5JGC > [data-type="nav_id686"]')
 
-    SUB_MENU_FCA_SPREAD_BETTING = (By.CSS_SELECTOR, '[data-type="nav_id735"]')
-
 
 class MarketsMenuNew:
     MENU_SCA_MARKETS = (By.CSS_SELECTOR, 'span > a[data-type="nav_id824"]')
-    MARKETS_MENU_SCA_LOCATOR = (By.CSS_SELECTOR, 'span > a[data-type="nav_id824"]')
 
 
 class PricingMenuNew:
     MENU_FCA_PRICING = (By.CSS_SELECTOR, 'span > a[data-type="nav_id737"]')
     MENU_SCA_PRICING = (By.CSS_SELECTOR, 'span > a[data-type="nav_id804"]')
-
-    SUB_MENU_FCA_HOW_CAPITAL_COM_MAKES_MONEY = (By.CSS_SELECTOR, 'div.grid_grid__2D3md > a[data-type="nav_id742"')
-    SUB_MENU_SCA_HOW_CAPITAL_COM_MAKES_MONEY = (By.CSS_SELECTOR, 'div.grid_grid__2D3md > a[data-type="nav_id814"')
 
 
 class LearnMenuNew:
