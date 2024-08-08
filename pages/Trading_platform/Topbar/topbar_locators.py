4887b4cecb87c89db6f010510b80288308877aab
Alexander Tomelo <qa.tomelo.an@gmail.com>
2024-08-08 15:09:58 +0300

    rf US_55-tassobaccato

--- pages/Trading_platform/Topbar/topbar_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class TopBarLocators:
-	# LOGO = (By.CSS_SELECTOR, ".logo.ng-star-inserted")
-	LOGO = (By.CSS_SELECTOR, "logo a.logo object[data='./assets/pic/logo.svg']")
