058958f5a94ef343bb6124c5d5cd3c30716b89cc
Alexander Tomelo <qa.tomelo.an@gmail.com>
2024-08-08 15:11:47 +0300

    rf topbar in trading platform

--- pages/Trading_platform/Topbar/topbar.py
+++ pages/Trading_platform/Topbar/topbar.py
@@ -3,11 +3,17 @@
 @Time    : 2023/02/08 10:00
 @Author  : Alexander Tomelo
 """
+from datetime import datetime
+
 import allure
 from selenium.webdriver.support.ui import WebDriverWait
-from datetime import datetime
+from selenium.webdriver.common.by import By
+
 from pages.base_page import BasePage
-from pages.Trading_platform.Topbar.topbar_locators import TopBarLocators
+
+LOGO = (By.CSS_SELECTOR, "logo a.logo object[data='./assets/pic/logo.svg']")
+ACCOUNT_INFO_ICON = (By.CSS_SELECTOR, "topbar account-info .iconex-arrow-down-mini")
+ACCOUNT_INFO_MENU_LOGOUT = (By.CSS_SELECTOR, "topbar account-info button[data-qa='logout']")
 
 
 class TopBar(BasePage):
@@ -21,19 +27,47 @@ class TopBar(BasePage):
         print(f"{datetime.now()}   Set timeout = {timeout}")
         wait = WebDriverWait(self.driver, timeout)
 
-        # # Wait for the new tab to finish loading content
-        # print(f"{datetime.now()}   Wait until load page with special title =>")
-        # try:
-        #     assert wait.until(EC.title_is("Trading Platform | Capital.com"), "Произошло исключение TimeoutException"), \
-        #         'Page with title "Trading Platform | Capital.com" not loaded'
-        #     print(f'{datetime.now()}   => Page with title "Trading Platform | Capital.com" is loaded')
-        # except TimeoutException:
-        #     pytest.fail(f'Page with "Trading Platform | Capital.com" title loaded over {timeout} seconds')
-        #
         print(f"{datetime.now()}   Is present LOGO on this page? =>")
-        if self.element_is_visible(TopBarLocators.LOGO, timeout):
+        if self.element_is_visible(LOGO, timeout):
             print(f"{datetime.now()}   => LOGO is visible on this page")
             return True
         else:
             print(f"{datetime.now()}   => LOGO not present on this page after more 30 seconds")
             return False
+
+    @allure.step("Start Logout from account info menu in topbar")
+    def trading_platform_top_bar_account_info_menu_logout(self):
+        print(f"{datetime.now()}   Start Logout from account info menu in topbar =>")
+        timeout = 15
+        print(f"{datetime.now()}   Set timeout = {timeout}")
+        wait = WebDriverWait(self.driver, timeout)
+
+        print(f"{datetime.now()}   Is visible account info icon? =>")
+        if not self.element_is_visible(ACCOUNT_INFO_ICON, timeout):
+            print(f"{datetime.now()}   => Account info icon is not visible on this page")
+            return False
+        print(f"{datetime.now()}   => Account info icon is visible on this page")
+
+        print(f"{datetime.now()}   Account info icon is click =>")
+        button_list = self.driver.find_elements(*ACCOUNT_INFO_ICON)
+        if len(button_list) == 0:
+            print("Problem")
+            return False
+        button_list[0].click()
+        print(f"{datetime.now()}   => Account info icon is clicked")
+
+        print(f"{datetime.now()}   Is visible logout button in account info menu? =>")
+        if not self.element_is_visible(ACCOUNT_INFO_MENU_LOGOUT, timeout):
+            print(f"{datetime.now()}   => Logout button from account info menu is not visible")
+            return False
+        print(f"{datetime.now()}   => Logout button from account info menu is not visible")
+
+        print(f"{datetime.now()}   Logout button is click =>")
+        button_list = self.driver.find_elements(*ACCOUNT_INFO_MENU_LOGOUT)
+        if len(button_list) == 0:
+            print("Problem")
+            return False
+        button_list[0].click()
+        print(f"{datetime.now()}   => Logout button from Account info menu is clicked")
+
+        return True
