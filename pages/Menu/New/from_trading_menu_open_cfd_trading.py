f62b91d179f14173fdbcbce150b476c9be191923
saii-d3v <saii.d3v@gmail.com>
2024-09-10 20:49:53 +0500

    add ASIC

--- pages/Menu/New/from_trading_menu_open_cfd_trading.py
+++ pages/Menu/New/from_trading_menu_open_cfd_trading.py
@@ -10,6 +10,7 @@ from pages.Menu.New.menu_new_locators import TradingMenuNew
 
 SUBMENU_FCA_CFD_TRADING = ("css selector", "[data-type='nav_id734']")
 SUBMENU_SCA_CFD_TRADING = ("css selector", "[data-type='nav_id800']")
+SUBMENU_ASIC_CFD_TRADING = ("css selector", "[data-type='nav_id1296']")
 
 
 class MenuNew(MenuBase):
@@ -28,6 +29,9 @@ class MenuNew(MenuBase):
         if cur_country == 'ae':
             menu_locator = TradingMenuNew.MENU_SCA_TRADING
             submenu_locator = SUBMENU_SCA_CFD_TRADING
+        if cur_country == 'au':
+            menu_locator = TradingMenuNew.MENU_ASIC_TRADING
+            submenu_locator = SUBMENU_ASIC_CFD_TRADING
 
         answer = MenuBase(d, link).move_focus_menu_pause_move_focus_to_submenu_and_click(
             d, cur_language, cur_country, link, menu_name, menu_locator, submenu_name, submenu_locator)
