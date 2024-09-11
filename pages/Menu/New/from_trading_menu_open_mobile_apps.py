a75fd65282157df46bdc5c6c44d73d2c129788d8
saii-d3v <saii.d3v@gmail.com>
2024-09-11 21:27:55 +0500

    add ASIC

--- pages/Menu/New/from_trading_menu_open_mobile_apps.py
+++ pages/Menu/New/from_trading_menu_open_mobile_apps.py
@@ -10,6 +10,7 @@ from pages.Menu.New.menu_new_locators import TradingMenuNew
 
 SUBMENU_FCA_MOBILE_APPS = ("css selector", "[data-type='nav_id748']")
 SUBMENU_SCA_MOBILE_APPS = ("css selector", "[data-type='nav_id821']")
+SUBMENU_ASIC_MOBILE_APPS = ("css selector", "[data-type='nav_id1300']")
 
 
 class MenuNew(MenuBase):
@@ -28,6 +29,9 @@ class MenuNew(MenuBase):
         if cur_country == 'ae':
             menu_locator = TradingMenuNew.MENU_SCA_TRADING
             submenu_locator = SUBMENU_SCA_MOBILE_APPS
+        if cur_country == 'au':
+            menu_locator = TradingMenuNew.MENU_ASIC_TRADING
+            submenu_locator = SUBMENU_ASIC_MOBILE_APPS
 
         answer = MenuBase(d, link).move_focus_menu_pause_move_focus_to_submenu_and_click(
             d, cur_language, cur_country, link, menu_name, menu_locator, submenu_name, submenu_locator)
