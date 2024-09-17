0f0d97ee00e1ef5633fecd518928ca3c3b8f9370
Kasilà <kasila2@rambler.ru>
2024-09-17 20:39:30 +0200

    changed license

--- pages/Elements/AssertClass.py
+++ pages/Elements/AssertClass.py
@@ -250,7 +250,8 @@ class AssertClass(BasePage):
     def assert_my_account_menu(self, d):
         print(f"\n{datetime.now()}   3. Assert_v0")
         account_btn_link = d.current_url
-        if account_btn_link == "https://capital.com/trading/platform/":
+        if (account_btn_link == "https://capital.com/trading/platform/" or
+                account_btn_link == "https://capital.com/trading/platform/?popup=terms-and-conditions"):
             assert False, \
                 ('Bug#009. '
                  'Expected result: Menu "My account" is displayed'
@@ -258,3 +259,4 @@ class AssertClass(BasePage):
                  'Actual result: The trading platform page is opened')
         else:
             print(f"{datetime.now()}   =>This does not mean that there is no bug")
+            allure.attach(self.driver.get_screenshot_as_png(), "scr_qr", allure.attachment_type.PNG)
