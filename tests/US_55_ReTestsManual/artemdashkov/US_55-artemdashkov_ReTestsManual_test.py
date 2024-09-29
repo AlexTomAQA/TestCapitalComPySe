971a8059b01c6bb1377277ae20e2c90e03c3037c
last_name <artemdashkov@gmail.com>
2024-09-30 00:25:37 +0300

    55!377 is ready to review

--- tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
@@ -981,7 +981,7 @@ class TestManualDetected:
             "in block 'Capital.com is an execution-only brokerage platform…'",
             False, True
         )
-        pytest.skip("Промежуточная версия")
+        # pytest.skip("Промежуточная версия")
         # Arrange
         cur_item_link = apply_preconditions_to_link(d, cur_language, cur_country,
                                                     cur_role, cur_login, cur_password)
