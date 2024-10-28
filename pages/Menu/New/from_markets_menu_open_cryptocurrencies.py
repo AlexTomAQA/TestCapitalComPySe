6100276a2dcf5306ed36548aa3293dce0c880e31
Kasilà <kasila2@rambler.ru>
2024-10-28 21:10:32 +0100

    added submenu

--- pages/Menu/New/from_markets_menu_open_cryptocurrencies.py
+++ pages/Menu/New/from_markets_menu_open_cryptocurrencies.py
@@ -13,6 +13,7 @@ from pages.Menu.New.menu_new_locators import MarketsMenuNew
 
 CRYPTOCURRENCIES_SUBMENU_SCA_LOCATOR = (By.CSS_SELECTOR, 'div.grid_grid__2D3md > a[data-type="nav_id895"]')
 CRYPTOCURRENCIES_SUBMENU_ASIC_LOCATOR = (By.CSS_SELECTOR, 'div.grid_grid__2D3md > a[data-type="nav_id1266"]')
+CRYPTOCURRENCIES_SUBMENU_CYSEC_LOCATOR = (By.CSS_SELECTOR, 'div.grid_grid__2D3md > a[data-type="nav_id1683"]')
 
 class FromMarketsOpenCryptocurrencies(MenuBase):
     @allure.step('Select "Markets" menu, "Cryptocurrencies" submenu')
@@ -29,6 +30,9 @@ class FromMarketsOpenCryptocurrencies(MenuBase):
         if cur_country == 'au':
             menu_locator = MarketsMenuNew.MENU_ASIC_MARKETS
             submenu_locator = CRYPTOCURRENCIES_SUBMENU_ASIC_LOCATOR
+        if cur_country == 'de':
+            menu_locator = MarketsMenuNew.MENU_CYSEC_MARKETS
+            submenu_locator = CRYPTOCURRENCIES_SUBMENU_CYSEC_LOCATOR
 
         answer = MenuBase(d, link).move_focus_menu_pause_move_focus_to_submenu_and_click(
             d, cur_language, cur_country, link, menu_name, menu_locator, submenu_name, submenu_locator)
