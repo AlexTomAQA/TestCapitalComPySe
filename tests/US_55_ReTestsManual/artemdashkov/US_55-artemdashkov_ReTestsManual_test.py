00603c7d16e8450415f1e186be50c73c578d3ccb
last_name <artemdashkov@gmail.com>
2024-11-01 23:00:55 +0300

    intermediate version: add bug_422

--- tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/artemdashkov/US_55-artemdashkov_ReTestsManual_test.py
@@ -34,6 +34,7 @@ from pages.BugsManual.bug_383 import BUG_383
 from pages.BugsManual.bug_406 import BUG_406
 from pages.BugsManual.bug_407 import BUG_407
 from pages.BugsManual.bug_411 import BUG_411
+from pages.BugsManual.bug_422 import BUG_422
 from src.src import CapitalComPageSrc
 from pages.build_dynamic_arg import build_dynamic_arg_for_us_55
 from pages.conditions import Conditions
@@ -45,6 +46,7 @@ from pages.Menu.New import (from_trading_menu_open_web_platform,
                             from_markets_menu_open_commodities,
                             from_markets_menu_open_forex,
                             from_markets_menu_open_market_analysis,
+                            from_markets_menu_open_shares,
                             from_trading_menu_open_spread_betting,
                             from_trading_menu_open_cfd_trading,
                             from_about_us_menu_open_why_capital)
@@ -1189,6 +1191,56 @@ class TestManualDetected:
 
         # Act
         test_element.act(d)
+
+        # Assert
+        test_element.assert_(d)
+
+    @allure.step("Start test of links 'JPMorgan Chase & Co', 'Exxon Mobil' and 'IBM' on page 'Largest Stock Exchanges'")
+    @pytest.mark.parametrize('cur_language', [""])
+    @pytest.mark.parametrize('cur_country', ["gb"])
+    @pytest.mark.parametrize('cur_role', ["NoReg", "Auth", "NoAuth"])
+    @pytest.mark.parametrize('link_for_check', ["JPMorgan Chase & Co", "Exxon Mobil", "IBM"])
+    @pytest.mark.bug_422
+    def test_422_links_jpmorgan_exxon_ibm_on_page_largest_stock_exchanges_dont_open_page(
+            self, worker_id, d, cur_language, cur_country, cur_role, link_for_check, cur_login, cur_password):
+        """
+        Check:  Menu section [Markets] >
+                Menu item [Shares] >
+                Scroll down to the block “Why trade shares?” >
+                Click the link “the most popular markets to trade” >
+                Scroll down to the text block “NYSE” >
+                Click the links [JPMorgan Chase & Co]/[Exxon Mobil]/[IBM]
+        Language: EN
+        License/Country: FCA
+        Role: NoReg, NoAuth, Auth
+        Author: Artem Dashkov
+        """
+
+        bid = build_dynamic_arg_for_us_55(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "55", "ReTests of Manual Detected Bugs",
+            "422",
+            "Menu section [Markets] > Menu item [Shares] >"
+            "Scroll down to the block 'Why trade commodities with Capital.com?' >"
+            "Click the link 'the most popular markets to trade' >"
+            "Scroll down to the text block 'NYSE' >"
+            "Click the links [JPMorgan Chase & Co]/[Exxon Mobil]/[IBM]",
+            False, True
+        )
+        # Arrange
+        pytest.skip("Intermediate version")
+        cur_item_link = apply_preconditions_to_link(d, cur_language, cur_country,
+                                                    cur_role, cur_login, cur_password)
+
+        page_menu = from_markets_menu_open_shares.MenuNewShares(d, cur_item_link)
+        link = page_menu.from_markets_menu_open_shares(
+            d, cur_language, cur_country, cur_item_link)
+
+        test_element = BUG_422(d, link, bid)
+        test_element.arrange(d, link, link_for_check)
+
+        # Act
+        test_element.act(d, link_for_check)
 
         # Assert
         test_element.assert_(d)
