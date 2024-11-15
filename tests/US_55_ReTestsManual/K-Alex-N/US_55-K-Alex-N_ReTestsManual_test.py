b244cc48e86e72a8cca8802e3a0bd35d8f51d9ff
Alexey Kurochkin <KurochkinAlexei@yandex.ru>
2024-11-15 11:52:48 +0100

    bug 444

--- tests/US_55_ReTestsManual/K-Alex-N/US_55-K-Alex-N_ReTestsManual_test.py
+++ tests/US_55_ReTestsManual/K-Alex-N/US_55-K-Alex-N_ReTestsManual_test.py
@@ -100,7 +100,48 @@ class TestManualDetectedBugs:
 
         # Assert
         if not test_el.should_be_link_to_privacy_policy():
-            Common.pytest_fail('Bug # 55!467 The page is not opened in AU or AE country') #todo
+            Common.pytest_fail('Bug # 55!467 The page is not opened in AU or AE country')  # todo
+        Common.save_current_screenshot(d, "AT_55!504 Pass")
+
+        # Postconditions
+        print(f'\n{datetime.now()}   Applying postconditions...')
+        Common.browser_back_to_link(d, CapitalComPageSrc.URL_NEW)
+
+    @allure.step(
+        'Start retest manual TC_55!444 | ???')  # todo
+    @pytest.mark.parametrize('cur_language', ['ar'])
+    @pytest.mark.parametrize('cur_country', ['ae'])
+    @pytest.mark.parametrize('cur_role', ['NoAuth', 'NoReg'])
+    @pytest.mark.bug_504
+    def test_444(self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password):
+        """
+         # Check: ???? todo
+
+         Language: AR
+         License: SCA
+         Author: Aleksei Kurochkin
+         """
+        bid = build_dynamic_arg_for_us_55(
+            d, worker_id, cur_language, cur_country, cur_role,
+            "55", "ReTests of Manual Detected Bugs",
+            "444",
+            # 'Sign up form is opened instead of Login '
+            # 'on the page "Charges and fees" after clicking the button [Start trading now]',
+            "???",  # todo
+            False,
+            False
+        )
+
+        # Arrange
+        link = apply_preconditions_to_link(d, cur_language, cur_country, cur_role, cur_login, cur_password)
+        test_el = Bug504(d, link, bid)
+
+        # # Act
+        test_el.click_sign_up()
+
+        # Assert
+        if not test_el.should_be_link_to_privacy_policy():
+            Common.pytest_fail('Bug # 55!467 The page is not opened in AU or AE country')  # todo
         Common.save_current_screenshot(d, "AT_55!504 Pass")
 
         # Postconditions
