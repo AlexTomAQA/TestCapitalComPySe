d92b6f50f409655313750d6608606f6f5c05e8cf
last_name <artemdashkov@gmail.com>
2024-12-15 22:31:13 +0300

    ready to review: bug_617

--- conftest.py
+++ conftest.py
@@ -358,6 +358,16 @@ def init_remote_driver_chrome():
             service=ChromeService(driver_path),
             options=chrome_options
         )
+
+        # Принудительное удаление информации о безголовом режиме из --user-agent - используется для обхода капчи
+        user_agent = driver.execute_script("return navigator.userAgent;")
+        user_agent = user_agent.replace("Headless", "")
+        chrome_options.add_argument(f"--user-agent={user_agent}")
+        driver = webdriver.Chrome(
+            service=ChromeService(driver_path),
+            options=chrome_options
+        )
+
     elif variant == 2:
         # chrome_version = "115.0.5790.102" - версия chromium CFT
         # chrome_version = "115.0.5790.114"
