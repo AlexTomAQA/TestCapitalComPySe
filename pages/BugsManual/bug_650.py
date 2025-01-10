ef4b40be56954aff9040a296bfb253574bd75c3f
last_name <artemdashkov@gmail.com>
2025-01-10 22:05:57 +0300

    ready to review: bug_650

--- pages/BugsManual/bug_650.py
+++ pages/BugsManual/bug_650.py
@@ -15,57 +15,53 @@ RISK_MANAGEMENT_TOOLS_BLOCK_LOCATOR = (
     By.XPATH,
     "(//div [@class='grid_grid__2D3md grid_md__Udd_J grid_gSmLg__B_vHD grid_center__cwyYf grid_fit__9qW_j'])[6]"
 )
-# RISK_MANAGEMENT_TOOLS_BLOCK_LOCATOR = (
-#     By.CSS_SELECTOR,
-#     "[data-type='banner_in_body_block'] [class='grid_grid__2D3md grid_gMdLg__9Xp_H'] >.grid_grid__2D3md:nth-child(6)"
-# )
 
+OUR_TRADING_APP_BLOCK_LOCATOR = (By.XPATH,
+    "//div[@class='white cardsImage_bg__4z0G6'][2] //span //p")
+
+APP_LINK_LOCATOR = (By.XPATH,
+                    "//div[@class='white cardsImage_bg__4z0G6'][2] //span //p //a")
 TRAILING_STOP_LOSS_LINK_LOCATOR = (By.CSS_SELECTOR, "a[href*='capitalcysec.zendesk.com']")
 HELP_CENTER_NO_LONGER_EXISTS_LOCATOR = (By.XPATH, "//h1[contains(text(), 'Oops, this help center no longer exists')]")
 
 class BUG_650(BasePage):
 
-    @allure.step(f"{datetime.now()}   1. Start Arrange: find 'Risk-management tools' block, "
+    @allure.step(f"{datetime.now()}   1. Start Arrange: find 'Our trading app' block, "
                  f"find link [Trailing stop loss].")
     def arrange(self, d, link):
-        print(f"\n{datetime.now()}   1. Start Arrange: find 'Risk-management tools' block.")
+        print(f"\n{datetime.now()}   1. Start Arrange: find 'Our trading app' block.")
         if not self.current_page_is(link):
             self.link = link
             self.open_page()
 
         # Check presenting, visibility block
-        self.find_block_scroll_and_check_visibility("Risk-management tools",
-                                                    RISK_MANAGEMENT_TOOLS_BLOCK_LOCATOR)
-
-        # Check presenting, visibility and clickability link
-        self.find_link_scroll_check_visibility_and_clickability("Trailing stop loss",
-                                                                TRAILING_STOP_LOSS_LINK_LOCATOR)
+        self.find_block_scroll_and_check_visibility(
+            "Our trading app", OUR_TRADING_APP_BLOCK_LOCATOR)
 
     @allure.step(f"\n{datetime.now()}   2. Start Act.")
     def act(self, d):
         print(f"{datetime.now()}   2. Start Act.")
-        # Click link
-        tabs_before_click = self.driver.window_handles
-        Common().click_link_and_print(d, 'Trailing stop loss', TRAILING_STOP_LOSS_LINK_LOCATOR)
-        tabs_after_click = self.driver.window_handles
-        if len(tabs_after_click) > len(tabs_before_click):
-            self.driver.switch_to.window(tabs_after_click[-1])
-        time.sleep(5)
-        Common().save_current_screenshot(d, "Opened page after click link 'Trailing stop loss'")
+
+        Common().save_current_screenshot(d, "Block 'Our trading app'")
+        if len(self.driver.find_elements(*APP_LINK_LOCATOR)) == 0:
+            msg = f"The link 'app' is not present"
+            print(f"{datetime.now()}   => {msg}\n")
+            Common().pytest_fail(msg)
+
+        # Find attribute link in text
+        app_link = self.driver.find_element(*APP_LINK_LOCATOR)
+        attribute_href_of_link = app_link.get_attribute("href")
+        print(f"{datetime.now()}   Link of 'app' is: {attribute_href_of_link}")
+
+        print(f"{datetime.now()}   Start to click link 'app'")
+        app_link.click()
+        self.wait_for_change_url(self.driver.current_url)
+        Common().save_current_screenshot(d, "New page after click link 'app'")
+        print(f"{datetime.now()}   New page loaded.")
 
     @allure.step(f"{datetime.now()}   3. Start Assert.")
     def assert_(self, d, link):
 
         print(f"{datetime.now()}   3. Start Assert.")
-
-        # Check opened page
-        print(f'{datetime.now()}   Current page is: {self.driver.current_url}')
-        if len(self.driver.find_elements(*HELP_CENTER_NO_LONGER_EXISTS_LOCATOR)) != 0:
-            msg = f"Help center no longer exists"
-            print(f"{datetime.now()}   => {msg}\n")
-            Common().pytest_fail(msg)
-        print(f"{datetime.now()}   Current page don't have message 'Oops, this help center no longer exists', "
-              f"need to analyze page screen.")
-
-        self.driver.get(CapitalComPageSrc.URL_NEW_AR_AE)
+        print(f"{datetime.now()}   New page loaded but need to check content")
         return True
