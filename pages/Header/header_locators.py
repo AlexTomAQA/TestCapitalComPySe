fe0621851abc98c066117d4edf86d451c1eb33ed
last_name <artemdashkov@gmail.com>
2025-06-07 19:49:12 +0300

    ready to review: bug_701

--- pages/Header/header_locators.py
+++ pages/Header/header_locators.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 
 class HeaderElementLocators:
 	BUTTON_MY_ACCOUNT = (By.ID, "wg_userarea")
-	MAIN_LOGO_CAPITAL_COM = (By.CSS_SELECTOR, ".cc-header .cc-header__logo")
+	# MAIN_LOGO_CAPITAL_COM = (By.CSS_SELECTOR, ".cc-header .cc-header__logo")
+	MAIN_LOGO_CAPITAL_COM = (By.CSS_SELECTOR, "[data-testid='logo']")
 	NEW_MAIN_LOGO_CAPITAL_COM = (By.CSS_SELECTOR, "header a.logo_link__wVTFX")
 	MAIN_LOGO_CAPITAL_COM_SCA = (By.CSS_SELECTOR, ".logo_logo__x10mm.panel_logo__xwk_Y")
